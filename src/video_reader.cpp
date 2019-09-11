e402cccf751e8b70337fc107afa87b47c6b74dd2
Roman Senn <roman.l.senn@gmail.com>
2019-09-11 16:06:14 +0200

    ...

--- src/video_reader.cpp
+++ src/video_reader.cpp
@@ -4,11 +4,9 @@
 #include <algorithm>
 #include <numeric>
 
-#include <opencv2/core/core.hpp>
-//#include <opencv2/core/opengl_interop.hpp>
-#include <opencv2/gpu/gpu.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/contrib/contrib.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
 
 int
 main(int argc, const char* argv[]) {
