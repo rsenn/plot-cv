5716614664542210b0388c9fcd55b7763e7fde31
Roman Senn <roman.l.senn@gmail.com>
2019-09-12 06:31:58 +0200

    ...

--- src/CannyDetector_Demo.cpp
+++ src/CannyDetector_Demo.cpp
@@ -25,44 +25,42 @@ const char* window_name = "Edge Map";
  * @function CannyThreshold
  * @brief Trackbar callback - Canny thresholds input with a ratio 1:3
  */
-static void CannyThreshold(int, void*)
-{
-    //![reduce_noise]
-    /// Reduce noise with a kernel 3x3
-    blur( src_gray, detected_edges, Size(3,3) );
-    //![reduce_noise]
+static void
+CannyThreshold(int, void*) {
+  //![reduce_noise]
+  /// Reduce noise with a kernel 3x3
+  blur(src_gray, detected_edges, Size(3, 3));
+  //![reduce_noise]
 
-    //![canny]
-    /// Canny detector
-    Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
-    //![canny]
+  //![canny]
+  /// Canny detector
+  Canny(detected_edges, detected_edges, lowThreshold, lowThreshold * ratio, kernel_size);
+  //![canny]
 
-    /// Using Canny's output as a mask, we display our result
-    //![fill]
-    dst = Scalar::all(0);
-    //![fill]
+  /// Using Canny's output as a mask, we display our result
+  //![fill]
+  dst = Scalar::all(0);
+  //![fill]
 
-    //![copyto]
-    src.copyTo( dst, detected_edges);
-    //![copyto]
+  //![copyto]
+  src.copyTo(dst, detected_edges);
+  //![copyto]
 
-    //![display]
-    imshow( window_name, dst );
-    //![display]
+  //![display]
+  imshow(window_name, dst);
+  //![display]
 }
-
 
 /**
  * @function main
  */
-int main( int argc, char** argv )
-{
+int
+main(int argc, char** argv) {
   //![load]
-  CommandLineParser parser( argc, argv, "{@input | fruits.jpg | input image}" );
-  src = imread( samples::findFile( parser.get<String>( "@input" ) ), IMREAD_COLOR ); // Load an image
+  CommandLineParser parser(argc, argv, "{@input | fruits.jpg | input image}");
+  src = imread(parser.get<String>("@input"), IMREAD_COLOR); // Load an image
 
-  if( src.empty() )
-  {
+  if(src.empty()) {
     std::cout << "Could not open or find the image!\n" << std::endl;
     std::cout << "Usage: " << argv[0] << " <Input image>" << std::endl;
     return -1;
@@ -71,20 +69,20 @@ int main( int argc, char** argv )
 
   //![create_mat]
   /// Create a matrix of the same type and size as src (for dst)
-  dst.create( src.size(), src.type() );
+  dst.create(src.size(), src.type());
   //![create_mat]
 
   //![convert_to_gray]
-  cvtColor( src, src_gray, COLOR_BGR2GRAY );
+  cvtColor(src, src_gray, COLOR_BGR2GRAY);
   //![convert_to_gray]
 
   //![create_window]
-  namedWindow( window_name, WINDOW_AUTOSIZE );
+  namedWindow(window_name, WINDOW_AUTOSIZE);
   //![create_window]
 
   //![create_trackbar]
   /// Create a Trackbar for user to enter threshold
-  createTrackbar( "Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold );
+  createTrackbar("Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold);
   //![create_trackbar]
 
   /// Show the image
