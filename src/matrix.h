dad8c63e0fe41636d2447728a0201cd21b5fdb94
Roman Senn <roman.l.senn@gmail.com>
2019-11-13 15:11:42 +0100

    ...

--- src/matrix.h
+++ src/matrix.h
@@ -2,11 +2,12 @@
 #include <opencv2/core/affine.hpp>
 #include <opencv2/imgproc/types_c.h>
 
-template <class T = double> class Matrix : public cv::Affine3<T> {
+template <class T = double> class Matrix : public cv::Mat_<T> {
 public:
-  typedef cv::Affine3<T> base_type;
-  Matrix() {}
-  Matrix(cv::Mat m) : cv::Affine3<T>(m) {}
+  typedef cv::Mat_<T> base_type;
+
+  Matrix() : base_type(3,3, std::is_same<T, double>::value ? CV_64F : CV_32F) {}
+  Matrix(cv::Mat m) : base_type(m) {}
 
   void
   transform_points(std::vector<cv::Point_<T>>& pt) {
@@ -22,7 +23,7 @@ public:
     pt = v[0];
   }
 
-  operator cv::Mat_<T>() const { return this->matrix; }
+  operator cv::Mat_<T>() const { return *this; }
 
   static Matrix<T>
   rotation(double angle) {
@@ -38,7 +39,7 @@ template <class T>
 inline std::string
 to_string(const Matrix<T>& mat) {
   std::ostringstream oss;
-  oss << "rows: " << mat.matrix.rows;
-  oss << " cols: " << mat.matrix.cols;
+  oss << "rows: " << mat.rows;
+  oss << " cols: " << mat.cols;
   return oss.str();
 }
