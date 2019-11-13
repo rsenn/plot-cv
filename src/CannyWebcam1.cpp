de24a917ee2fd9c614dcaf6e6b378b5eaf6649e5
Roman Senn <roman.l.senn@gmail.com>
2019-11-13 02:19:46 +0100

    ...

--- src/CannyWebcam1.cpp
+++ src/CannyWebcam1.cpp
@@ -547,14 +547,13 @@ main(int argc, char* argv[]) {
 
   cv::VideoCapture capWebcam;
 
-/*
   capWebcam.open(camID, cv::CAP_V4L2); // declare a VideoCapture object and associate to webcam, 0 => use 1st webcam
 
   if(capWebcam.isOpened() == false) { // check if VideoCapture object was associated to webcam successfully
     std::cout << "error: capWebcam not accessed successfully\n\n"; // if not, print error message to std out
     getchar();                                                     // may have to modify this line if not using Windows
     return (0);                                                    // and exit program
-  }*/
+  }
 
   cv::Mat imgRaw, imgOriginal, imgTemp, imgGrayscale, imgBlurred, imgCanny; // Canny edge image
 
@@ -576,8 +575,8 @@ main(int argc, char* argv[]) {
   while(charCheckForEscKey != 27 /*&& capWebcam.isOpened()*/) { // until the Esc key is pressed or webcam connection is lost
     bool blnFrameReadSuccessfully = true;
 
-    //    blnFrameReadSuccessfully = capWebcam.read(imgRaw); // get next frame
-    imgRaw = cv::imread("input.png");
+        blnFrameReadSuccessfully = capWebcam.read(imgRaw); // get next frame
+   // imgRaw = cv::imread("input.png");
 
     if(!blnFrameReadSuccessfully || imgRaw.empty()) {     // if frame not read successfully
       std::cout << "error: frame not read from webcam\n"; // print error message to std out
