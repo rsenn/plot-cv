abb6069ce3d83b8fc667ede0a6e7d4ef775dcd7e
Roman Senn <roman.l.senn@gmail.com>
2019-11-13 21:35:20 +0100

    ...

--- src/CannyWebcam1.cpp
+++ src/CannyWebcam1.cpp
@@ -386,12 +386,11 @@ std::vector<cv::Point2f>
 get_mass_centers(std::vector<PointVec> contours) {
   std::vector<cv::Moments> mu(contours.size());
   std::vector<cv::Point2f> mc(contours.size());
-  for(size_t i = 0; i < contours.size(); i++) {
+  for(size_t i = 0; i < contours.size(); i++) 
     mu[i] = cv::moments(contours[i], false);
-  }
-  for(size_t i = 0; i < contours.size(); i++) {
+  for(size_t i = 0; i < contours.size(); i++)
     mc[i] = cv::Point2f(mu[i].m10 / mu[i].m00, mu[i].m01 / mu[i].m00);
-  }
+
   return mc;
 }
 
