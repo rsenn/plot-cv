48d53309522d7bed6dcf2471f0229dd47e130fb5
Roman Senn <roman.l.senn@gmail.com>
2019-11-13 19:56:48 +0100

    ...

--- src/matrix.h
+++ src/matrix.h
@@ -9,7 +9,7 @@ public:
 
   static const int typeId = std::is_same<T, double>::value ? CV_64F : CV_32F;
 
-  Matrix() : base_type() { *this = Matrix<T>::identity(); }
+  Matrix() : base_type(cv::Mat::zeros(3,3, typeId)) { }
   Matrix(const base_type& m) : base_type() { base_type::operator=(m); }
   Matrix(const typed_type& m) : base_type() { base_type::operator=(m); }
   template <class OtherT> Matrix(const Matrix<OtherT>& m) : base_type() { base_type::operator=(m); }
