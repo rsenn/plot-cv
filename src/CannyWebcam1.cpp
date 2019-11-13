3ddada97e6cac26e3870b8908033cbea9a4176b0
Roman Senn <roman.l.senn@gmail.com>
2019-11-13 15:27:29 +0100

    ...

--- src/CannyWebcam1.cpp
+++ src/CannyWebcam1.cpp
@@ -889,6 +889,7 @@ main(int argc, char* argv[]) {
       auto t = Matrix<double>::translation(10,50);
       cout << "matrix scale " << to_string(s) << endl;
       cout << "matrix rotate " << to_string(r) << endl;
+      cout << "matrix translate " << to_string(t) << endl;
       }
 
       cout << "histogram:";
