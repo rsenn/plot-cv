0950fcc36fd52206d9a04ecf7313727a981ba0a3
Roman <roman.l.senn@gmail.com>
2020-03-30 05:19:05 +0200

    ...

--- test-autoplacer.js
+++ test-autoplacer.js
@@ -1,10 +1,10 @@
-import Autoplacer from './lib/autoplacer/autoplacer.js';
-import { BBox } from './lib/dom/bbox.js';
-import { TRBL } from './lib/dom/trbl.js';
-import { Rect } from './lib/dom/rect.js';
-import { Alea } from './lib/alea.js';
+import Autoplacer from "./lib/autoplacer/autoplacer.js";
+import { BBox } from "./lib/dom/bbox.js";
+import { TRBL } from "./lib/dom/trbl.js";
+import { Rect } from "./lib/dom/rect.js";
+import { Alea } from "./lib/alea.js";
 
-import { Console } from 'console';
+import { Console } from "console";
 
 global.console = new Console({ stdout: process.stdout, stderr: process.stderr, inspectOptions: { depth: 10, colors: true } });
 
@@ -14,30 +14,27 @@ var bb = new BBox();
 
 var rects = [];
 
-for(var i =  0; i < 10; i++) {
-  let r = new Rect(0,0, Math.round(rg() * 100), Math.round(rg() * 100));
+for (var i = 0; i < 10; i++) {
+  let r = new Rect(0, 0, Math.round(rg() * 100), Math.round(rg() * 100));
 
   rects.push(r);
 }
 
 var bl = rects.map(rect => {
-  const {left,top} = new TRBL(...rect.points());
-  const {width,height} = rect;
-  console.log({left,top});
-  return [left,top,width,height];
+  const { left, top } = new TRBL(...rect.points());
+  const { width, height } = rect;
+  console.log({ left, top });
+  return [left, top, width, height];
 });
 
-
-console.log("rg:",rg());
-console.log("bb:",bb);
-console.log("bl:",bl);
+console.log("rg:", rg());
+console.log("bb:", bb);
+console.log("bl:", bl);
 
 var ap = new Autoplacer({ bodies: bl });
 
-
-console.log("ap.next():",ap.next());
+console.log("ap.next():", ap.next());
 
 ap.next();
 
-console.log("ap:",ap.bodies_);
-
+console.log("ap:", ap.bodies_);
