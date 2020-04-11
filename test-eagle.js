b5c5659e2421f6bf133e6a398620a5a073026516
Roman <roman.l.senn@gmail.com>
2020-04-11 14:06:21 +0200

    ...

--- test-eagle.js
+++ test-eagle.js
@@ -16,20 +16,32 @@ function dump(obj, depth = 2, breakLength = 400) {
   return util.inspect(obj, { depth, colors: true, breakLength });
 }
 
+function xmlize(obj, depth = 2) {
+  return obj.toXML ? obj.toXML() : EagleDocument.toXML(obj, depth).split(/\n/g)[0];
+}
+
 function* testEagle(filename) {
   let proj = new EagleProject(filename);
   let { board, schematic, libraries } = proj;
 
+
+
   /*console.log("libraries:", dump(proj.libraries));*/
 
+  let elements = [...board.getAll("element", (value,path,hier) => new EagleEntity(board, path, value))];
+  let parts = [...schematic.getAll("part",  (value,path,hier) => new EagleEntity(board, path, value))];
+    console.log("elements:\n  "+elements.map((element,i) => `element #${i}: `+xmlize(element, 0)).join("\n  "));
+    console.log("parts:\n  "+parts.map((part,i) => `part #${i}: `+xmlize(part, 0)).join("\n  "));
 
-  let elems = board.get("elements") || board.get("parts");
+  
+
+
 
   let named = [...board.findAll(e => e.attributes && "name" in e.attributes)];
   let withPackage = [...board.findAll(e => e.attributes && "device" in e.attributes)];
   let parents = [...board.findAll(e => e.children instanceof Array && e.children.length > 0)];
   let tags = named.map(e => EagleDocument.toXML(e));
-/*
+  /*
  libraries = [
     ...board.findAll(e => e.tagName == "library" && e.attributes && "name" in e.attributes)
   ];
@@ -46,8 +58,7 @@ function* testEagle(filename) {
 
   elems.forEach(elem => {
     //console.log(`${elem.tagName}:`, elem.toXML(0), elem.attributes.package);
-    if(elem.deviceset)
-      console.log(`${elem.tagName}.deviceset:`, elem.deviceset.toXML(1), elem.attributes.deviceset);
+    if(elem.deviceset) console.log(`${elem.tagName}.deviceset:`, elem.deviceset.toXML(1), elem.attributes.deviceset);
   });
   false &&
     named.forEach(([e, p]) => {
