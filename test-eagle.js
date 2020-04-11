9d0bfb8bc996cc7533fed8fb67a21fcc4051d40c
Roman <roman.l.senn@gmail.com>
2020-04-11 16:07:48 +0200

    ...

--- test-eagle.js
+++ test-eagle.js
@@ -2,6 +2,7 @@ import fs from "fs";
 import { EagleEntity } from "./lib/eagle/entity.js";
 import { EagleDocument } from "./lib/eagle/document.js";
 import { EagleProject } from "./lib/eagle/project.js";
+import { EagleLocator } from "./lib/eagle/locator.js";
 import Util from "./lib/util.js";
 import util from "util";
 import { Console } from "console";
@@ -20,25 +21,37 @@ function xmlize(obj, depth = 2) {
   return obj.toXML ? obj.toXML().replace(/>\s*</g, ">\n    <") : EagleDocument.toXML(obj, depth).split(/\n/g)[0];
 }
 
-function* testEagle(filename) {
+function testLocator() {
+  let l = new EagleLocator([1, 2, 3, 4, 5, 6]);
+  let a = [l.slice(), l.slice()];
+  console.log("l:", dump(l));
+  console.log("a[0] == a[1]:", a[0] === a[1]);
+  a[1][0] = "x";
+  console.log("a:", dump(a));
+  let b = [l.prevSibling, l.nextSibling, l.parent];
+  console.log("b:", dump(b));
+  console.log(b[2].parent.parent.up(3));
+}
+
+async function testEagle(filename) {
   let proj = new EagleProject(filename);
   let { board, schematic, libraries } = proj;
 
-
   /*console.log("libraries:", dump(proj.libraries));*/
 
-  let elements = [...board.getAll("element", (value,path,hier) => new EagleEntity(board, path, value))];
-  let parts = [...schematic.getAll("part",  (value,path,hier) => new EagleEntity(board, path, value))];
+  let elements = [...board.getAll("element", (value, path, hier) => new EagleEntity(board, path, value))];
+  let parts = [...schematic.getAll("part", (value, path, hier) => new EagleEntity(board, path, value))];
   let descriptions = [...schematic.getAll("description")];
 
+  // console.log("descriptions:",descriptions.map(([value,path,hier]) => value));
+  console.log("elements:\n  " + elements.map((element, i) => `element #${i}: ` + xmlize(element, 0)).join("\n  "));
+  console.log("parts:\n  " + parts.map((part, i) => `part #${i}: ` + xmlize(part, 0)).join("\n  "));
+    //  console.log("devices:\n  "+dump([...schematic.getAll("device",  (value,path,hier) => value /*new EagleEntity(board, path, value)*/)], 10, 200));
+  console.log("devicesets:\n  " + [...schematic.getAll("deviceset", (value, path, hier, doc) => (path))].join("\n"));
 
-   // console.log("descriptions:",descriptions.map(([value,path,hier]) => value));
-    console.log("elements:\n  "+elements.map((element,i) => `element #${i}: `+xmlize(element, 0)).join("\n  "));
-    console.log("parts:\n  "+parts.map((part,i) => `part #${i}: `+xmlize(part, 0)).join("\n  "));
+  return proj.saveTo(".", true);
 
-  
-
-/*
+  /*
 
   let named = [...board.findAll(e => e.attributes && "name" in e.attributes)];
   let withPackage = [...board.findAll(e => e.attributes && "device" in e.attributes)];
@@ -48,16 +61,14 @@ function* testEagle(filename) {
   let nodes = EagleDocument.traverse(board.xml[0]);
 */
   //console.log("library", dump(libraries, 2));
-
+  /*
   for(let [node, path, hier] of EagleDocument.traverse(board.xml[0])) {
     let pathstr = EagleDocument.nodeName(node, path, hier);
     yield [board.type + "/" + pathstr, EagleDocument.toXML(node, 0)];
   }
+  return;*/
 
-  
-  fs.writeSync(process.stdout.fd, "board:\n"+ board.toString());
-
-  return;
+  fs.writeSync(process.stdout.fd, "board:\n" + board.toString());
 
   elems.forEach(elem => {
     //console.log(`${elem.tagName}:`, elem.toXML(0), elem.attributes.package);
@@ -69,7 +80,7 @@ function* testEagle(filename) {
       //console.log("e:", elem.toXML(), elem.attributes.package);
     });
 }
-let result = testEagle("../an-tronics/eagle/Headphone-Amplifier-ClassAB-alt3");
 
-[...result];
+testEagle("../an-tronics/eagle/Headphone-Amplifier-ClassAB-alt3").then(result => console.log(result));
 //for(let elem of result) console.log(elem.join("\t"));
+testLocator();
