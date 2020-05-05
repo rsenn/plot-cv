6d4400388f6bc0153ea82bd2223e85449434ea7f
Roman <roman.l.senn@gmail.com>
2020-05-05 12:39:03 +0200

    ...

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,37 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
+    node: true,
   },
-  extends: "eslint:recommended",
+  extends: [
+    'airbnb',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
+    ecmaFeatures: {
+      modules: true,
+      jsx: true,
+    },
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module',
   },
-  rules: {}
+  settings: {
+    react: {
+      pragma: 'h',
+    },
+  },
+  plugins: ['react'],
+  rules: {
+    'no-unused-vars': ['error', { varsIgnorePattern: 'h' }],
+    'no-console': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+  },
 };
