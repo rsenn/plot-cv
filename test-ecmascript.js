0cfaad8110ed1892b58f7b76bc6697b33c9016b6
Roman <roman.l.senn@gmail.com>
2020-05-09 02:44:39 +0200

    ...

--- test-ecmascript.js
+++ test-ecmascript.js
@@ -1,11 +1,12 @@
 import Parser from "./lib/ecmascript/parser.js";
-import Lexer, { Stack } from "./lib/ecmascript/lexer.js";
+import Lexer, { Stack, PathReplacer } from "./lib/ecmascript/lexer.js";
 import Printer from "./lib/ecmascript/printer.js";
 import Util from "./lib/util.js";
 import fs from "fs";
 import util from "util";
 import { Console } from "console";
 import { estree, Factory } from "./lib/ecmascript/estree.js";
+//import process from 'process';
 
 global.console = new Console({
   stdout: process.stdout,
@@ -21,12 +22,43 @@ if(args.length == 0) args.push("-");
 
 let files = args.reduce((acc, arg) => ({ ...acc, [arg]: undefined }), {});
 
+
+process.on('uncaughtException', (err, origin) => {
+  fs.writeSync(
+    process.stderr.fd,
+    `Caught exception: ${err}\n
+Exception origin: ${origin}\n
+Stack: ${err.stack}`
+  );
+           process.exit();
+
+});
+
+process.on("SIGINT", () => {
+ //fs.writeSync(process.stderr.fd, "\nSIGINT - Exit\n");
+    console.log( "\nSIGINT - Exit\n");
+
+  finish();
+      process.exit(3);
+
+});
+
+process.on("exit", () => {
+   //fs.writeSync(process.stderr.fd, "\nexited\n");
+   console.log("\nexited\n");
+         process.exit();
+
+ });
+
+
+main(args);
+
 /*
 const LoginIcon = ({ style }) => (<svg style={style} height="56" width="34" viewBox="0 0 8.996 14.817" xmlns="http://www.w3.org/2000/svg">
     <defs />
 
 */
-const dumpFile = (name, data) => {
+function dumpFile(name, data) {
   if(Util.isArray(data)) data = data.join("\n");
   if(typeof data != "string") data = "" + data;
 
@@ -37,48 +69,76 @@ const dumpFile = (name, data) => {
 
 Error.stackTraceLimit = 100;
 
-(async (...args) => {
+global.parser = null;
+
+
+
+function main(args) {
   if(args.length == 0) args.push("./lib/ecmascript/parser.js");
 
   for(let arg of args) {
     let data, b, ret;
     if(arg == "-") arg = "/dev/stdin";
 
-    data = await fs.promises.readFile(arg);
+    console.log("file:", arg);
+
+    data = fs.readFileSync(arg);
     console.log("opened:", data);
 
-    let ast, fail;
+    let ast, error;
     console.log(estree.ArrayBindig);
 
     console.log(Util.getCallerStack());
 
+    global.parser = new Parser(data.toString(), arg);
+
     try {
-      ast = await Parser.parse(data.toString(), arg);
+      ast = parser.parseProgram();
     } catch(err) {
-      console.log(err.toString());
-
-      let stack = err.stack && err.stack.length ? err.stack[0] : err.stack;
-      console.log(err.stack);
-
-      console.log(Parser.instance.trace() || err.stack);
-      let lexer = Parser.instance.lexer;
-      let t = [];
-
-      // console.log("currentLine:", lexer.currentLine());
-
-      dumpFile("trace.log", Parser.instance.trace());
-      fail = true;
-
-      files[arg] = !fail;
-
-      let printer = new Printer({ indent: 4 });
-
-      console.log("\nAST:\n   " + printer.print(ast).replace(/\n/g, "\n  "));
-
-      console.log("finish: " + (fail ? "error" : "success"));
+      error = err;
     }
+
+    files[arg] = finish(error);
+
     console.log("files:", files);
 
-    process.exit(Number(files.length == 0));
+    //  process.exit(Number(files.length == 0));
   }
-})(...args);
+}
+
+function finish(err) {
+
+  if(err) {
+    console.log("ERROR: "+Util.className(err)+": "+err.toString());
+  }
+
+  let fail = !!err;
+
+//  let stack =/* (err instanceof SyntaxError  && err.stack.length) ? err.stack : */
+ if(fail) {
+ let stack = PathReplacer()(''+err.stack);
+  console.log("STACK:\n"+stack);
+}
+
+  let lexer = parser.lexer;
+  let t = [];
+
+  console.log(parser.trace() );
+
+  // console.log("currentLine:", lexer.currentLine());
+
+  dumpFile("trace.log", parser.trace());
+
+ 
+
+//  let printer = new Printer({ indent: 4 });
+
+  if(fail) { console.log("\nerror:", err.msg, "\n", parser.lexer.currentLine());
+  } else {
+  /*  let out = printer.print(ast);
+    console.log("\nAST:\n   " + out.replace(/\n/g, "\n  "));*/
+  }
+
+  console.log("finish: " + (fail ? "error" : "success"));
+  return !fail;
+}
