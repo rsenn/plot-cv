65bed0cac383d7a8002cbf65156687d1b8bb8e23
Roman <roman.l.senn@gmail.com>
2020-05-11 09:38:01 +0200

    ...

--- test-template.js
+++ test-template.js
@@ -1,3 +1,8 @@
 const value = 42;
 const word = `simple`;
 const simple = `this is a ${word}    ${value * 3} template`;
+
+const pushHandler = /*async */ state => {
+  console.log(`loading "${name}"...`);
+  await load(name);
+};
