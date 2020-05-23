0c8e2efbee1f756015f3910558e7e83d0b9f35dc
Roman <roman.l.senn@gmail.com>
2020-05-23 06:17:20 +0200

    ...

--- test-renderer.js
+++ test-renderer.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import { EagleDocument } from './lib/eagle/document.js';
 import { SchematicRenderer } from './lib/eagle/renderer.js';
 import { ReactComponent } from './lib/dom/preactComponent.js';
+import { Circle } from './lib/geom/circle.js';
 import { h, render, Component } from './node_modules/htm/preact/standalone.mjs';
 
 let doc = new EagleDocument(fs.readFileSync('Headphone-Amplifier-ClassAB-alt3.sch').toString());
@@ -22,7 +23,14 @@ let renderer = new SchematicRenderer(doc, (tag, attrs, parent) => {
 //let svg = h('svg', {});
 let output = renderer.render();
 
+let instances = renderer.renderInstances();
+
 console.log('output:', output);
+
+let circle = new Circle(0, 0, 10);
+
+console.log('circle:', circle);
+console.log('circle:', circle.bbox());
 
 fs.writeFileSync('output.svg', ReactComponent.stringify(output));
 //console.log("output:",ReactComponent.stringify(svg));
