375349d47458ba3aadda6c35f982ec2c068bfb92
Roman <roman.l.senn@gmail.com>
2020-05-23 10:59:41 +0200

    ...

--- test-clipper.js
+++ test-clipper.js
@@ -1,5 +1,7 @@
 import ClipperLib from './lib/clipper-lib.js';
 import SvgPath from './lib/svg/path.js';
+import { Point, PointList } from './lib/geom.js';
+import { SVG } from './lib/dom.js';
 import { parse, parseSVG } from './lib/svg/path-parser.js';
 import { Console } from 'console';
 import Shape from './lib/clipper.js';
@@ -13,10 +15,14 @@ global.console = new Console({
 const d =
   'M 193.54706,178.86683 163.80521,218.90155 116.21174,233.8085 68.945718,217.89373 40.061173,177.23615 40.591015,127.36556 70.332862,87.330839 117.92634,72.423889 165.19236,88.338658 194.0769,128.99624 Z';
 
-const data = parse(d).filter(({ x, y }) => x !== undefined);
+const d2 =
+  'M6.13 26.94L16.33 4.5l4.887 25.657 13.16-26.689 5.545 1.948 14.276 4.896-18.561 8.397-7.08 15.744 30.796-4.765 8.73-18.562-1.895-3.904.087-.066';
+
+const data = new PointList(SVG.pathParse(d).filter(({ x, y }) => x !== undefined && y !== undefined));
+const data2 = new PointList(parse(d2).filter(({ x, y }) => x !== undefined && y !== undefined));
 
 function testOffset() {
-  const path = data.map(({ x, y }) => new ClipperLib.DoublePoint(x, y));
+  const path = data2.map(({ x, y }) => new ClipperLib.DoublePoint(x, y));
   //console.log('ClipperLib:', ClipperLib);
 
   let area = ClipperLib.JS.AreaOfPolygon(path);
@@ -26,11 +32,15 @@ function testOffset() {
 
   const offset = new ClipperLib.ClipperOffset();
   const outer = new ClipperLib.Paths();
-  offset.AddPath(path, ClipperLib.JoinType.jtRound, ClipperLib.EndType.etClosedPolygon);
+  offset.AddPath(path, ClipperLib.JoinType.jtRound, ClipperLib.EndType.etOpenRound);
 
-  offset.Execute(outer, 10);
-  console.log('testOffset:', { path, area, bounds, lightened, outer });
+  offset.Execute(outer, 1);
+
+  let points = new PointList(outer[0].map(({ X, Y }) => new Point(X, Y)));
+  console.log('data2:', data2.toPath());
+  console.log('points:', points.toPath());
 }
+
 function testClipper() {
   const subjectPaths = [
     [
@@ -59,7 +69,7 @@ function testClipper() {
 }
 
 function testShape() {
-  let shape = new Shape([data], true, true);
+  let shape = new Shape([data2], true, true);
 
   let area = shape.totalArea();
   let bounds = shape.shapeBounds();
@@ -67,18 +77,21 @@ function testShape() {
   let rounded = shape.round().mapToLower();
   let outset = shape.offset(10, {
     jointType: 'jtSquare',
-    endType: 'etClosedPolygon',
+    endType: 'etOpenSquare',
     miterLimit: 2.0,
     roundPrecision: 0.25
   });
-  let inset = shape.offset(-10, {
+  let points = new PointList(outset.paths[0].map(({ X, Y }) => new Point(X, Y)));
+  /*  let inset = shape.offset(-10, {
     jointType: 'jtSquare',
-    endType: 'etClosedPolygon',
+    endType: 'etOpenSquare',
     miterLimit: 2.0,
     roundPrecision: 0.25
-  });
+  });*/
 
-  console.log('testShape:', { area, bounds, simplified, rounded, outset, inset });
+  let path = points.toPath();
+
+  console.log('testShape:', { path, outset });
 }
 
 testOffset();
--- test-renderer.js
+++ test-renderer.js
@@ -7,30 +7,29 @@ import { h, render, Component } from './node_modules/htm/preact/standalone.mjs';
 
 let doc = new EagleDocument(fs.readFileSync('Headphone-Amplifier-ClassAB-alt3.sch').toString());
 
-console.log('bounds:', doc.getBounds());
+//console.log('bounds:', doc.getBounds());
 
 let renderer = new SchematicRenderer(doc, (tag, attrs, parent) => {
   let elem = h(tag, attrs);
-
   if(parent) {
-    if(parent.props.children instanceof Array) parent.props.children.push(elem);
-    else if(parent.props.children) parent.props.children = [parent.props.children, elem];
-    else parent.props.children = elem;
+    const { props } = parent;
+    if(props.children instanceof Array) props.children.push(elem);
+    else if(props.children) props.children = [props.children, elem];
+    else props.children = elem;
   }
   return elem;
 });
 
-//let svg = h('svg', {});
-let output = renderer.render();
+let output = renderer.render(null, null, 0);
 
-let instances = renderer.renderInstances();
+//let instances = renderer.renderInstances();
 
 console.log('output:', output);
-
+/*
 let circle = new Circle(0, 0, 10);
 
 console.log('circle:', circle);
-console.log('circle:', circle.bbox());
+console.log('circle:', circle.bbox());*/
 
 fs.writeFileSync('output.svg', ReactComponent.stringify(output));
 //console.log("output:",ReactComponent.stringify(svg));
