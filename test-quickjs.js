9fdc77aab42a3d9ada473dadac460ff181d479dd
Roman <roman.l.senn@gmail.com>
2020-06-11 08:59:56 +0200

    ...

--- test-quickjs.js
+++ test-quickjs.js
@@ -4,31 +4,28 @@ import { Size } from './build/x86_64-linux-gnu/quickjs-size.so';
 import { Mat } from './build/x86_64-linux-gnu/quickjs-mat.so';
 import { PointIterator } from './build/x86_64-linux-gnu/quickjs-point-iterator.so';
 import { inspect } from './inspect.js';
-import { Contour } from "./build/x86_64-linux-gnu/quickjs-contour.so";
+import { Contour } from './build/x86_64-linux-gnu/quickjs-contour.so';
 console.log('test:', inspect({ Point, Size, Rect, Mat, PointIterator, Contour }));
 
+let c = new Contour();
 
-let  c = new Contour();
-
-c.push(new Point(0,0));
-c.push(new Point(10,0));
-c.push(new Point(20,0));
-c.push(new Point(30,0));
-c.push(new Point(30,10));
-c.push(new Point(30,20));
-c.push(new Point(30,30));
-c.push(new Point(20,30));
-c.push(new Point(10,30));
-c.push(new Point(0,30));
-c.push(new Point(0,20));
-c.push(new Point(0,10));
-c.push(new Point(0,0));
+c.push(new Point(0, 0));
+c.push(new Point(10, 0));
+c.push(new Point(20, 0));
+c.push(new Point(30, 0));
+c.push(new Point(30, 10));
+c.push(new Point(30, 20));
+c.push(new Point(30, 30));
+c.push(new Point(20, 30));
+c.push(new Point(10, 30));
+c.push(new Point(0, 30));
+c.push(new Point(0, 20));
+c.push(new Point(0, 10));
+c.push(new Point(0, 0));
 
 console.log('contour:', inspect(c));
 
-
 for(let point of c) {
-  let {x,y} = point;
-  console.log('point:', point, x,y);
-
+  let { x, y } = point;
+  console.log('point:', point, x, y);
 }
