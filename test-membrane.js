aefd27e29d5a1aa5f13907bf0b45ed9eb619af9c
Roman <roman.l.senn@gmail.com>
2020-06-26 23:10:58 +0200

    ...

--- test-membrane.js
+++ test-membrane.js
@@ -5,15 +5,18 @@ import Util from './lib/util.js';
 import fs from 'fs';
 import { Path, PathMapper, toXML, TreeObserver, findXPath } from './lib/json.js';
 import { Console } from 'console';
+
 const printNode = node => {
   let s = toXML(node).replace(/\n.*/g, '');
   return s;
 };
+
 const CH = 'children';
+
 global.console = new Console({
   stdout: process.stdout,
   stderr: process.stderr,
-  inspectOptions: { depth: 2, colors: true }
+  inspectOptions: { depth: 0, colors: true }
 });
 
 Error.stackTraceLimit = 100;
@@ -33,32 +36,33 @@ try {
 
     let path2obj = deep.flatten(
       xml,
-      {},
+      new Map(),
       (v, p, r) => typeof v == 'object' && v !== null && v.tagName !== undefined,
-      (p, v) => obj2path(v, p), //.xpath(xml)
+      (p, v) => obj2path(v, p),
       ({ tagName, attributes, children, ...value }) => (children.length ? [tagName, attributes, children] : [tagName, attributes])
     );
-    let flat = new Map(Object.entries(path2obj));
 
-    console.log('flat:', path2obj['children.0.children.0.children.3.children.8.children.302.children.1']);
-    //console.log('flat:', path2obj);
-    let signals = findXPath('/eagle/drawing/board/signals/signal', flat, { root: xml, recursive: false, entries: false });
-    let all = findXPath('//board/[^/]*/?[^/]*', flat, { root: xml, recursive: false, entries: false });
-    let k = [...all.keys()];
-    console.log('signals:', signals);
-    console.log('all:', k); //.map(k => (k+'').replace(/.*\//g, "")));mk
+    let flat = deep.flatten(
+      xml,
+      new Map(),
+      (v, p, r) => typeof v == 'object' && v !== null && v.tagName !== undefined,
+      (p, v) => obj2path(v, p),
+      (v, p) => v
+    );
+
+    //    let flat = path2obj instanceof Map ? path2obj : new Map(Object.entries(path2obj));
+
     let rel,
       prev = new Path([], true),
       prevParent,
       relTo = [];
     const mk = ([path, value]) => {
-      path = path.join('/').replace(/\/\[/g, '[');
+      path = [...path].join('/').replace(/\/\[/g, '[');
 
       let p = new Path(path, true);
-      //((prev||[]).slice(0, -1)+'');
+      console.log('mk=', { path, p });
 
       let thisParent = p.parent;
-     // console.log('mk=', { thisParent, p, prev, prevParent });
       if(thisParent.equal(prev)) path = p.relativeTo(thisParent);
 
       if(prev.equal(thisParent)) {
@@ -79,58 +83,59 @@ try {
       prevParent = thisParent;
       return path;
     };
-    console.log('drawing:', findXPath('//drawing', flat, { root: xml, entries: true, recursive: true }).map(mk));
+    console.log('drawing:', findXPath('//drawing', flat, { root: xml, entries: true, recursive: false }).map(mk));
 
     //process.exit(0);
-    let p = treeObserve.get(path2obj);
+    let p = treeObserve.get(Object.fromEntries(flat.entries()));
     let node = p;
     let unwrapped, type, path;
-    treeObserve.subscribe((what, target, key, path) => {
+
+    treeObserve.subscribe((what, target, key, p, value) => {
       if(what == 'access') return;
-      console.log('event', what, key, path);
-      //    if(mapper === null) process.exit(1);
+      if(target[key] === undefined) return;
+      let [path, k] = p;
+      path = new Path(path);
+      let targetType = treeObserve.getType(target);
+      let targetKeys = Object.keys(target).join(',');
+      let valueType = typeof value;
+      console.log(`event`, Util.toString({ what,/* targetType, key, targetKeys, */ valueType, 
+        xpath: path.xpath(xml)+'', string: typeof value == 'string' ? value : '' }));
     });
     mapper.set(treeObserve.unwrap(node), []);
-    for(let [path, obj] of flat) {
+
+    for(let [path, obj] of path2obj) {
+      //   console.log('path2obj:', { path, obj });
+
       let [tagName, attributes, children] = obj;
       if(path == '0') path = '';
       path = new Path(path, true);
-      //console.log('path:', path.xpath(xml));
+
+      obj.attributes;
+
       if(Object.keys(attributes).length == 0) continue;
 
       let xpath = path2xpath(obj2path(path.apply(xml)));
-      console.log('element:', /*path,*/ xpath + '', toXML({ tagName, ...attributes }, 1, ''));
     }
-    while(node) {
-      unwrapped = treeObserve.unwrap(node);
-      type = treeObserve.getType(node);
-      path = treeObserve.getXPath(node).toString();
-      if(Util.isObject(node)) {
-        if(!Util.isObject(node.attributes)) node.attributes = {};
-      }
-      const { x1, x2, y1, y2 } = node.attributes;
-      if(x1 !== undefined) console.log('x1,x2,y1,y2', { x1, x2, y1, y2 });
-      console.log('tagName:', node.tagName);
-      console.log('type:', type, path);
-      if(node.children === undefined || !node.children.length) break;
-      node = node.children[node.children.length - 1];
+    for(let path in p) {
+      let obj = p[path];
+      let { tagName, attributes, children } = obj;
+      path = new Path(path, true);
+      let str = path.toString('/');
+      if(str == '0') str = '';
+      else if(str.length > 0 && /[0-9]/.test(str[0])) str = '/children/' + str;
+      else if(str.length > 0 && !str.startsWith('/')) str = '/' + str;
+      let o = p[str];
+      if(o === undefined) continue;
+      let keys = Object.keys(o);
+      if(o.attributes) o.attributes.test = 'AAA';
     }
     let found = deep.find(xml, (node, path) => node.tagName !== undefined && ['parts', 'instances', 'elements'].indexOf(node.tagName) != -1);
-    node = flat.get(found.path); //.value || mapper.atlat.(found.path);
-    console.log('p:', printNode(p));
+    node = flat.get(found.path);
     console.log('found:', found);
     console.log('node:', node);
     let xpath;
     xpath = mapper.xpath(unwrapped);
     let r = Path.parseXPath('[0]');
-    //console.log('xpath:', [...r]);
-    //console.log('xpath:', r.toString());
-    console.log('xpath r', mapper.xpath(unwrapped).toString());
-    console.log('result', r);
-    p = Path.parseXPath('/eagle/drawing/board/signals');
-    console.log('mapper', p);
-    p = Path.parseXPath("/eagle/drawing/board/signals/signal[@name='N$10']");
-    console.log('mapper', p);
   }
   main(...process.argv.slice(2));
 } catch(err) {
