396032bb9cc6c4a452082c113bf5c730062ddc62
Roman <roman.l.senn@gmail.com>
2020-09-11 18:13:20 +0200

    ...

---
+++ test-stream.js
@@ -0,0 +1,16 @@
+import Util from './lib/util.js';
+import PortableFileSystem from './lib/filesystem.js';
+import { WritableStream } from './lib/stream/writableStream.js';
+
+let filesystem;
+
+async function main() {
+  await PortableFileSystem((fs) => (filesystem = fs));
+
+  let s = await filesystem.open('tmp/7seg-2.54.brd');
+  let r = await filesystem.read(s);
+
+  ws = await filesystem.open('tmp/test.txt', 'w');
+}
+
+Util.callMain(main, true);
