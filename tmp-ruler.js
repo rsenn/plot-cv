c7b4c45db3f4a15c3809ad8cbeeb80fde74941a9
Roman <roman.l.senn@gmail.com>
2020-11-26 17:19:08 +0100

    ...

---
+++ tmp-ruler.js
@@ -0,0 +1,34 @@
+import { h, useState, useRef } from './lib/dom/preactComponent.js';
+
+export function Ruler() {
+  const refRuler = useRef();
+  const [value, setValue] = useState(null);
+
+  const pressingDown = () => refRuler.current.pressingDown();
+  const pressingUp = () => refRuler.current.pressingUp();
+  const stopPressing = () => refRuler.current.stopPressing();
+  const onChanged = value => setValue(value);
+
+  return h(RulerDraggable, {}, [
+    h('button', {
+        onMouseDown: pressingDown,
+        onMouseUp: stopPressing
+      }, [Down]
+    ),
+    h('button', {
+        onMouseDown: pressingUp,
+        onMouseUp: stopPressing
+      }, [Up]
+    ),
+    h('div', {}, [value]),
+    h(Ruler, {
+        ref: refRuler,
+        defaultValue: 50,
+        onChanged,
+        longLength: 300,
+        shortLength: 60,
+        horizontal: true
+      }, []
+    )
+  ]);
+}
