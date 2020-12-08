0b9f1b70c2434216125893af38f9d45384b89c5a
Roman <roman.l.senn@gmail.com>
2020-12-08 05:15:14 +0100

    ...

---
+++ read-deps.js
@@ -0,0 +1,79 @@
+import PortableFileSystem from './lib/filesystem.js';
+import Util from './lib/util.js';
+import ConsoleSetup from './lib/consoleSetup.js';
+import path from './lib/path.js';
+import PortableChildProcess from './lib/childProcess.js';
+
+let filesystem,
+  childProcess,
+  documents = [];
+
+function WriteFile(name, data) {
+  if(Util.isArray(data)) data = data.join('\n');
+  if(typeof data != 'string') data = '' + data;
+  filesystem.writeFile(name, data + '\n');
+  console.log(`Wrote ${name}: ${data.length} bytes`);
+}
+
+Util.define(Array.prototype, {
+  contains(arg) {
+    return this.indexOf(arg) != -1;
+  }
+});
+
+async function DumpDeps(sources, includeDirs = []) {
+  //let stderr = filesystem.open('deps.err', 'w+');$
+  let includes = includeDirs.reduce((acc, dir) => (/^\/opt/.test(dir) ? [...acc, '-isystem', dir] : [...acc, `-I${dir}`]),
+    []
+  );
+  let argv = ['-MM', ...includes, '-c', ...sources];
+  console.log('argv:', argv.join(' '));
+  let proc = childProcess('g++', argv, {
+    block: false,
+    stdio: [null, 'pipe', 'pipe']
+  });
+  let out = filesystem.readAll(proc.stdout);
+  let err = filesystem.readAll(proc.stderr);
+  filesystem.close(proc.stdout);
+  filesystem.close(proc.stderr);
+  out = out.replace(/\\\n\s*/g, '');
+  WriteFile('deps.mk', out);
+  let i = 0;
+  let ret = [];
+  let lines = out.split(/\n/g);
+  for(let line of lines) {
+    let source = sources[i++];
+    let idx = line.indexOf(': ');
+    if(idx == -1) continue;
+    let obj = line.substring(0, idx - 2);
+    let deps = line.substring(idx + 2).split(/\s+/g);
+    if(deps[0] == source) deps.shift();
+    deps.sort();
+    deps = deps.map(dep => path.normalize(dep));
+    ret.push([source, deps]);
+  }
+  return new Map(ret);
+}
+
+async function main(...sources) {
+  await ConsoleSetup({ colors: true, depth: 8, breakLength: 138 });
+  await PortableFileSystem(fs => (filesystem = fs));
+  await PortableChildProcess(cp => (childProcess = cp));
+
+  const env = await Util.env;
+  const includes = ['quickjs', 'src', '/opt/opencv-4.5.0/include', '/opt/opencv-4.5.0/include/opencv4'];
+
+  if(sources.length == 0) {
+    let files = filesystem.readdir('src').map(file => `src/${file}`);
+    sources = files.filter(path => /\.[ch]/.test(path));
+    sources.sort();
+  }
+
+  console.log(`DumpDeps(`, sources, `)`);
+
+  let deps = await DumpDeps(sources, includes);
+
+  console.log(`DumpDeps() = `, deps);
+}
+
+Util.callMain(main, true);
