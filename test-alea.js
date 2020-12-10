48c2b2ceb47d9466f85e229ad6c09e1d0f6db463
Roman <roman.l.senn@gmail.com>
2020-12-10 15:14:59 +0100

    ...

--- test-alea.js
+++ test-alea.js
@@ -1,11 +1,11 @@
-const Alea = require('./lib/alea.js');
-const Util = require('./lib/util.cjs').Util;
+import Alea from './lib/alea.js';
+import Util from './lib/util.js';
 
 let rng = new Alea(1337);
 
-//console.log(Util.repeat(10, rng));
-//console.log(Util.repeat(10, rng.fract53));
-//console.log(Util.repeat(10, rng.uint32));
-//console.log(Util.repeat(10, rng.int32));
-//console.log(Util.repeat(10, rng.signed));
-//console.log(Util.repeat(10, rng.color));
+console.log(Util.repeat(10, rng));
+console.log(Util.repeat(10, rng.fract53));
+console.log(Util.repeat(10, rng.uint32));
+console.log(Util.repeat(10, rng.int32));
+console.log(Util.repeat(10, rng.signed));
+console.log(Util.repeat(10, rng.color));
