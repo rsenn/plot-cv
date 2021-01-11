8a97b61dcc80cbb92d04895ab50acf9001ca003a
Roman <roman.l.senn@gmail.com>
2021-01-11 14:11:17 +0100

    ...

--- fire.js
+++ fire.js
@@ -1,8 +1,11 @@
 import { crosskit, CANVAS } from './lib/crosskit.js';
 import { RGBA } from './lib/color.js';
+import Util from './lib/util.js';
 
-const w = 640;
-const h = 480;
+Object.assign(globalThis, { RGBA, Util });
+
+const w = 320;
+const h = 200;
 
 crosskit.init({
   renderer: CANVAS,
@@ -21,24 +24,43 @@ crosskit.rect({
   angle: 0
 });
 
-const buffer = new ArrayBuffer(w * (h + 1));
+const buffer = new ArrayBuffer(w * (h + 2));
 const palette = CreatePalette().map(color => color.toCSS());
 //console.log('Create palette:', palette);
 
-function Fire() {
-  const pixels = Array.from({ length: h + 1 }).map((v, i) => new Uint8ClampedArray(buffer, (h - i) * w, w));
+const pixels = Array.from({ length: h + 2 }).map((v, i) => new Uint8ClampedArray(buffer, i * w, w)
+);
+const { now, waitFor, animationFrame } = Util;
+const fps = 10;
 
+Object.assign(globalThis, { buffer, palette, pixels, fps });
+
+async function Loop() {
+  const delay = 1000 / fps;
+  let prev = 0;
+
+  for(;;) {
+    Fire();
+    await animationFrame(delay);
+  }
+}
+
+function Fire() {
   for(let x = 0; x < w; x++) {
-    pixels[0][x] = RandomByte() | 0xc0;
+    pixels[h][x] = (RandomByte() & 0b10111111) | 0x40;
+    pixels[h + 1][x] = (RandomByte() & 0b10111111) | 0x40;
   }
 
-  for(let y = 1; y <= h; y++) {
+  for(let y = h / 2; y < h; y++) {
     for(let x = 0; x < w; x++) {
-      const value = [pixels[y - 1][x - 1], pixels[y - 1][x], pixels[y - 1][x + 1], pixels[y][x]].reduce((a, p) => a + (p | 0),
-        0
-      );
+      const sum = [
+        pixels[y + 1][Modulo(x - 1, w)],
+        pixels[y + 1][x],
+        pixels[y + 1][Modulo(x + 1, w)],
+        pixels[y + 2][x]
+      ].reduce((a, p) => a + (p | 0), 0);
 
-      pixels[y][x] = value >>> 2;
+      pixels[y][x] = (sum * 15) >>> 6;
     }
   }
 
@@ -47,7 +69,7 @@ function Fire() {
       crosskit.pixel({
         x,
         y,
-        color: palette[pixels[h - y][x]]
+        color: palette[pixels[y][x]]
       });
     }
   }
@@ -67,6 +89,61 @@ function CreatePalette() {
   return colors;
 }
 
+// For random numbers, use "x = 181 * x + 359" from
+// Tom Dickens "Random Number Generator for Microcontrollers"
+// https://web.archive.org/web/20170323204917/http://home.earthlink.net/~tdickens/68hc11/random/68hc11random.html
+let scratch = 0;
+
 function RandomByte() {
-  return Math.round(Math.random() * 255);
+  const value = 181 * scratch + 359;
+  scratch = value >>> 0;
+  return (value >>> 8) & 0xff;
 }
+
+function Modulo(n, m) {
+  return ((n % m) + m) % m;
+}
+
+let element, rect;
+
+function MouseHandler(e) {
+  const { offsetX, offsetY, target, buttons, type } = e;
+
+  if(type != 'click' && buttons == 0) return;
+
+  const x = Math.round((offsetX * w) / rect.width);
+  const y = Math.round((offsetY * h) / rect.height);
+
+  //console.log(`${e.type} @ ${x},${y}`);
+
+  const rc = RandomByte() | 0x80;
+
+  for(let ty = y - 1; ty < y + 1; ty++)
+    for(let tx = x - 1; tx < x + 1; tx++)
+      pixels[ty][tx] = rc;
+  
+  pixels[y + 1][x] = rc;
+}
+
+function ResizeHandler(e) {
+  rect = element.getBoundingClientRect();
+}
+
+Object.assign(globalThis, { RandomByte });
+
+window.addEventListener('load', () => {
+  element = document.querySelector('canvas');
+
+  rect = element.getBoundingClientRect();
+
+  const handler =
+    MouseHandler ||
+    Util.instrument(MouseHandler, (duration, name, args, ret) =>
+      console.log(`handler time: ${duration}`)
+    );
+
+  element.addEventListener('click', handler);
+  element.addEventListener('mousemove', handler);
+});
+
+Loop();
