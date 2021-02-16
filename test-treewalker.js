ed9d8efa6edba1c8c3f949649221544c8512ef80
Roman Senn <roman.l.senn@gmail.com>
2021-02-16 02:56:55 +0100

    ...

--- test-treewalker.js
+++ test-treewalker.js
@@ -17,45 +17,40 @@ async function main(...args) {
   console.log('result:', result);
 
   let walk = new TreeWalker(result[0]);
- console.log('walk:', walk.toString());
+  console.log('walk:', walk.toString());
+   let i = 0;
+  console.log('~TreeWalker.MASK_PRIMITIVE:', TreeWalker.MASK_PRIMITIVE.toString(2));
+  console.log(' TreeWalker.MASK_ALL:', TreeWalker.MASK_ALL);
+  console.log(' TreeWalker.MASK_ALL:', TreeWalker.MASK_ALL.toString(2));
+  walk.tagMask = TreeWalker.MASK_PRIMITIVE;
+  walk.tagMask = TreeWalker.MASK_OBJECT;
+  walk.flags = 0;
+  const { flags, tagMask } = walk;
+  console.log(' walk', { flags, tagMask });
+  while(walk.nextNode((v, k, w) => typeof v == 'object' && v instanceof Array && v.length > 0)) {
 
-  /*  console.log('root:', walk.root);
-  console.log('length:', walk.length);
-  console.log('key:', walk.key);
-  console.log('firstChild:', walk.firstChild());
-  do {
-    const { key, currentNode, index } = walk;
-    console.log('walk', { key, currentNode, index });
-  } while(walk.nextSibling());
-  console.log('length:', walk.length);
-  console.log('depth:', walk.depth);
-  console.log('index:', walk.index);*/
-let i= 0;
-walk.tagMask = 0b111011110;
- console.log('~TreeWalker.MASK_PRIMITIVE:', TreeWalker.MASK_PRIMITIVE.toString(2));
- console.log(' TreeWalker.MASK_ALL:', TreeWalker.MASK_ALL);
- console.log(' TreeWalker.MASK_ALL:', TreeWalker.MASK_ALL.toString(2));
+    console.log('type:',
+      typeof walk.currentNode,
+      'path:',
+      walk.currentPath.join('.').replace(/\.?children\.?/g, '/'),
+      typeof walk.currentNode != 'object' ? walk.currentNode : ''
+    );
+    let node = walk.currentNode;
 
-//walk.tagMask = 0xffff;
-walk.tagMask = TreeWalker.MASK_PRIMITIVE;
- //walk.expr = "^(-?[0-9.]*)$";
- while(walk.nextNode()) {
- //console.log('walk:', walk.toString());
-   // const { currentKey, currentPath, currentNode, index, depth } = walk;
-   // console.log('nextNode:', { i, index, depth, currentKey, currentPath, currentNode });
-
-   console.log('type:',  typeof walk.currentNode, 'path:', walk.currentPath.join('.').replace(/\.?children\.?/g, "/"), typeof walk.currentNode != 'object' ? walk.currentNode : '');
-let node = walk.currentNode;
-
-if(typeof node == 'object') {
-   console.log('attributes:', Object.getOwnPropertyNames(node).filter(n => typeof node[n] != 'object').reduce((acc,name) => ({ ...acc, [name]: node[name] }), {}) );
- }
+    if(typeof node == 'object') {
+      console.log('object:',
+        console.inspect(node, { depth: 0, colors: true }) ||
+          Object.getOwnPropertyNames(node)
+            .filter(n => typeof node[n] != 'object')
+            .reduce((acc, name) => ({ ...acc, [name]: node[name] }), {})
+      );
+    }
     i++;
   }
 
- console.log('walk:', walk.toString());
+  console.log('walk:', walk.toString());
 
- await import('std').then(std => std.gc());
+  await import('std').then(std => std.gc());
 }
 
 Util.callMain(main, true);
