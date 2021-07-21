b86372a35dc27026145b43e33b1115c8d25931f9
Roman Senn <roman.l.senn@gmail.com>
2021-07-21 02:46:08 +0200

    ...

--- test-rpc.js
+++ test-rpc.js
@@ -153,6 +153,8 @@ function main(...args) {
   });
 
   define(globalThis, listen ? { server: cli, cli } : { client: cli, cli });
+  delete globalThis.debug;
+  Object.defineProperty(globalThis, 'debug', { get: DebugFlags });
 
   if(listen) cli.listen(createWS, os);
   else cli.connect(createWS, os);
