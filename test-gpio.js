8240c6a96cd99b8413f9c711edca2670d21cd7f9
Roman <roman.l.senn@gmail.com>
2021-08-13 00:59:48 +0200

    ...

--- test-gpio.js
+++ test-gpio.js
@@ -2,14 +2,19 @@ import { GPIO } from 'gpio';
 
 function main(...args) {
   const gpio = new GPIO();
- console.log('gpio.initPin',gpio.initPin);
+  console.log('gpio.initPin', gpio.initPin);
+  console.log('gpio.setPin', gpio.setPin);
+  console.log('gpio.getPin', gpio.getPin);
+  console.log('gpio', Object.getOwnPropertyNames(gpio));
+  console.log('gpio.constructor', gpio.constructor);
+  console.log('GPIO', Object.getOwnPropertyNames(GPIO));
 
   gpio.initPin(0, GPIO.OUTPUT);
   gpio.setPin(0, 1);
 
   gpio.initPin(1, GPIO.INPUT);
- const value =  gpio.getPin(1);
- console.log('Pin #1 value:',value);
+  const value = gpio.getPin(1);
+  console.log('Pin #1 value:', value);
 }
 
 try {
