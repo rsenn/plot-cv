77e01ff3fefe9f97ec3589ed41cdea02b273af74
Roman Senn <roman.l.senn@gmail.com>
2021-09-24 01:19:24 +0200

    ...

---
+++ scripts/find-sources.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+ME=$0
+MYDIR=`dirname "$0"`
+THISDIR=${MYDIR%/scripts}
+
+find_sources() {
+   if [ $# -le 0 ]; then
+     SAVE_PWD=`pwd`
+     cd "$THISDIR"
+     for SUBDIR in qjs-* quickjs quickjs/qjs-*; do
+       test -d "$SUBDIR" &&
+       set -- "$@" "$THISDIR/$SUBDIR"
+     done
+     cd "$SAVE_PWD"
+   fi
+
+   find "$@" -maxdepth 1 -name "*.[ch]" -or -name "*.[ch]pp"
+}
+
+find_sources "$@"
