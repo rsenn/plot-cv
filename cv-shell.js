28ccf3b42484b6aaef1f42fa20cf5ea82c9af287
Roman Senn <roman.l.senn@gmail.com>
2021-10-26 16:18:46 +0200

    ...

--- cv-shell.js
+++ cv-shell.js
@@ -10,7 +10,7 @@ import REPL from './xrepl.js';
 import * as fs from './lib/filesystem.js';
 import { Pointer } from './lib/pointer.js';
 import * as Terminal from './terminal.js';
-import { as fromXML as toXML } from './lib/xml.js';
+import { read as fromXML, write as toXML } from './lib/xml.js';
 import inspect from './lib/objectInspect.js';
 import { ReadFile, LoadHistory, ReadJSON, MapFile, ReadBJSON, WriteFile, WriteJSON, WriteBJSON, DirIterator, RecursiveDirIterator, ReadDirRecursive } from './io-helpers.js';
 import { VideoSource, ImageSequence } from './qjs-opencv/js/cvVideo.js';
@@ -137,11 +137,6 @@ function main(...args) {
     TextStyle,
     DrawText,
     repl,
-    ReadDirRecursive,
-    Filter,
-    FilterImages,
-    StatFiles,
-    SortFiles,
     Util,
     toArrayBuffer,
     toString,
