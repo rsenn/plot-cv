a35c640c9dd02201842ef567e9d6d554dbd831a7
Roman Senn <roman.l.senn@gmail.com>
2021-12-04 05:44:48 +0100

    ...

--- list-directory.js
+++ list-directory.js
@@ -1,42 +1,4 @@
-import {
-  LogWrap,
-  VfnAdapter,
-  VfnDecorator,
-  Memoize,
-  DebugFlags,
-  Mapper,
-  DefaultConstructor,
-  EventLogger,
-  MessageReceiver,
-  MessageTransmitter,
-  MessageTransceiver,
-  RPCApi,
-  RPCProxy,
-  RPCObject,
-  RPCFactory,
-  Connection,
-  RPCServer,
-  RPCClient,
-  RPCSocket,
-  isThenable,
-  hasHandler,
-  callHandler,
-  parseURL,
-  GetProperties,
-  GetKeys,
-  getPropertyDescriptors,
-  define,
-  setHandlers,
-  statusResponse,
-  objectCommand,
-  MakeListCommand,
-  getPrototypeName,
-  SerializeValue,
-  DeserializeSymbols,
-  DeserializeValue,
-  RPCConnect,
-  RPCListen
-} from './quickjs/qjs-net/rpc.js';
+import { LogWrap, VfnAdapter, VfnDecorator, Memoize, DebugFlags, Mapper, DefaultConstructor, EventLogger, MessageReceiver, MessageTransmitter, MessageTransceiver, RPCApi, RPCProxy, RPCObject, RPCFactory, Connection, RPCServer, RPCClient, RPCSocket, isThenable, hasHandler, callHandler, parseURL, GetProperties, GetKeys, getPropertyDescriptors, define, setHandlers, statusResponse, objectCommand, MakeListCommand, getPrototypeName, SerializeValue, DeserializeSymbols, DeserializeValue, RPCConnect, RPCListen } from './quickjs/qjs-net/rpc.js';
 //import * as React from './lib/dom/preactComponent.js';
 import Util from './lib/util.js';
 
