cc7dacfb060b879ebf521b442a7f3c1337279746
Roman Senn <roman.l.senn@gmail.com>
2021-12-04 05:43:23 +0100

    ...

--- list-directory.js
+++ list-directory.js
@@ -1,4 +1,42 @@
-import { LogWrap, VfnAdapter, VfnDecorator, Memoize, DebugFlags, Mapper, DefaultConstructor, EventLogger, MessageReceiver, MessageTransmitter, MessageTransceiver, RPCApi, RPCProxy, RPCObject, RPCFactory, Connection, RPCServer, RPCClient, RPCSocket, isThenable, hasHandler, callHandler, parseURL, GetProperties, GetKeys, getPropertyDescriptors, define, setHandlers, statusResponse, objectCommand, MakeListCommand, getPrototypeName, SerializeValue, DeserializeSymbols, DeserializeValue, RPCConnect, RPCListen } from './quickjs/qjs-net/rpc.js';
+import {
+  LogWrap,
+  VfnAdapter,
+  VfnDecorator,
+  Memoize,
+  DebugFlags,
+  Mapper,
+  DefaultConstructor,
+  EventLogger,
+  MessageReceiver,
+  MessageTransmitter,
+  MessageTransceiver,
+  RPCApi,
+  RPCProxy,
+  RPCObject,
+  RPCFactory,
+  Connection,
+  RPCServer,
+  RPCClient,
+  RPCSocket,
+  isThenable,
+  hasHandler,
+  callHandler,
+  parseURL,
+  GetProperties,
+  GetKeys,
+  getPropertyDescriptors,
+  define,
+  setHandlers,
+  statusResponse,
+  objectCommand,
+  MakeListCommand,
+  getPrototypeName,
+  SerializeValue,
+  DeserializeSymbols,
+  DeserializeValue,
+  RPCConnect,
+  RPCListen
+} from './quickjs/qjs-net/rpc.js';
 //import * as React from './lib/dom/preactComponent.js';
 import Util from './lib/util.js';
 
@@ -7,23 +45,17 @@ window.addEventListener('load', () => {
 
   console.log('Loaded', url);
 
-  let ws = globalThis.ws = new WebSocket(url);
-ws.onopen = e => {
-  console.log('WS established',e);
-
-}
-ws.onmessage = e => {
-console.log('WS message',e);
-
-}
-ws.onclose = e => {
-console.log('WS close',e);
-
-}
-ws.onerror = e => {
-console.log('WS error',e);
-
-}
-
-
+  let ws = (globalThis.ws = new WebSocket(url));
+  ws.onopen = e => {
+    console.log('WS established', e);
+  };
+  ws.onmessage = e => {
+    console.log('WS message', e);
+  };
+  ws.onclose = e => {
+    console.log('WS close', e);
+  };
+  ws.onerror = e => {
+    console.log('WS error', e);
+  };
 });
