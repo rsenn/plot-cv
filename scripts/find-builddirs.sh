f1650f6b8b08f72d26c953434ff1d96c860ad096
Roman Senn <roman.l.senn@gmail.com>
2022-03-30 19:13:10 +0200

    ...

--- scripts/find-builddirs.sh
+++ scripts/find-builddirs.sh
@@ -1,6 +1,31 @@
 #!/bin/sh
-MYDIR=`dirname "$0"`
-PDIR=`dirname "$MYDIR"`
+
+MYDIR="`dirname "$0"`"
+PDIR="`dirname "$MYDIR"`"
 #echo "PDIR='$PDIR'" 1>&2
-ls -d $(find ${@:-$PDIR} -type d -name build | sed -u 's|$|/*-*-*/|') 2>/dev/null |
-  sed -u 's|^\./||'
+
+DIRS=
+OPTS=
+NL="
+"
+while [ $# -gt 0 ]; do
+  if [ -d "$1" ]; then
+    DIRS="${DIRS:+$DIRS$NL}$1"
+    shift
+    continue
+  fi
+    break
+done
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -name|-iname) OPTS="${OPTS:+$OPTS$NL}${1/name/wholename}$NL$2/CMakeCache.txt"
+      shift ;;
+    *) OPTS="${OPTS:+$OPTS$NL}$1" ;;
+  esac
+    shift
+done
+
+
+BUILDDIRS=$(ls -d $(find ${DIRS:-$PDIR} -type d -name build | sed -u 's|$|/*-*-*/|') 2>/dev/null | sed -u 's|^./||')
+
+exec find $BUILDDIRS $OPTS -type f -name CMakeCache.txt -exec dirname {} \;
