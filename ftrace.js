59d25b38280d66c6fcc129793af23bebdf2daf73
Roman Senn <roman.l.senn@gmail.com>
2022-10-10 02:52:40 +0200

    ...

---
+++ ftrace.js
@@ -0,0 +1,55 @@
+import fs from 'fs';
+import path from 'path';
+import { Console } from 'console';
+import { unescape } from 'misc';
+import { IfDebug, LogIfDebug, ReadFd, ReadFile, LoadHistory, ReadJSON, ReadXML, MapFile, WriteFile, WriteJSON, WriteXML, ReadBJSON, WriteBJSON, DirIterator, RecursiveDirIterator, ReadDirRecursive, Filter, FilterImages, SortFiles, StatFiles, FdReader, CopyToClipboard, ReadCallback, LogCall, Spawn, FetchURL } from './io-helpers.js';
+
+const StrDecode = s => ((s = s.slice(1, -1)), unescape(s));
+const StrFmt = s => (s[0] == '"' && s[s.length - 1] == '"' ? StrDecode(s) : s == 'NULL' ? null : !isNaN(+s) ? +s : s);
+const allFiles = new Set();
+
+function main(...args) {
+  globalThis.console = new Console({ inspectOptions: { compact: 2, customInspect: true, maxArrayLength: 200 } });
+
+  if(!args.length) args = ['strace.log'];
+
+  for(let logfile of args) {
+    let fd = fs.fopen(logfile, 'r');
+    let line;
+    console.log('fd', fd);
+
+    while((line = fd.getline())) {
+      let argIndex = line.indexOf('(');
+      let fnIndex = line.substring(0, argIndex).indexOf(' ');
+      let fnName = line.substring(fnIndex + 1, argIndex);
+
+      if(argIndex == -1) continue;
+      let resultIndex = line.indexOf(' = ');
+
+      let argLastIndex = line.substring(0, resultIndex != -1 ? resultIndex : line.length).indexOf(')');
+
+      let args = line
+        .substring(argIndex + 1, argLastIndex)
+        .split(/, /g)
+        .map(StrFmt);
+      let result = resultIndex != -1 ? line.substring(resultIndex + 3) : null;
+
+      const re = /([^\(]*)\(([^,]+(\)|,\s*)*|\s+=\s+)/g;
+
+      const strings = args.filter(arg => typeof arg == 'string');
+      const files = strings.filter(str => !allFiles.has(str) && fs.existsSync(str));
+      //console.log(fnName, args);
+      //console.log('result', result);
+      if(files) {
+        //console.log('files', files);
+        for(let file of files) allFiles.add(file);
+      }
+    }
+  }
+  const sorted = [...allFiles].sort();
+
+  console.log('allFiles', sorted.join('\n'));
+  WriteFile('output.files', sorted.join('\n'));
+}
+
+main(...scriptArgs.slice(1));
