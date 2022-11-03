2330f75e3dc2bf1ff5e036a46db6c67d8793c990
Roman Senn <roman.l.senn@gmail.com>
2022-11-04 00:21:04 +0100

    ...

--- test-worker.js
+++ test-worker.js
@@ -8,25 +8,49 @@ import { WorkerScript } from './workerScript.js';
 var worker;
 var counter;
 
-function TestWorker() {
-  globalThis.console = new Console({
-    colors: true,
-    compact: 1,
-    prefix: '\x1b[38;5;220mPARENT\x1b[0m'
-  });
+globalThis.console = new Console({
+  colors: true,
+  compact: 1,
+  prefix: '\x1b[38;5;220mPARENT\x1b[0m'
+});
 
+function TestWorker() {
   //worker = new os.Worker('./ws-worker.js');
   worker = new WorkerScript(`
 import { client, server, fetch, setLog, LLL_USER, LLL_NOTICE } from 'net';
 import * as std from 'std';
 import * as os from 'os';
 import { Console } from 'console';
+import { ProcessPool } from './process-pool.js';
+
+globalThis.console = new Console({
+  colors: true,
+  compact: 2,
+  prefix: '\x1b[38;5;220mCHILD\x1b[0m'
+});
 
 var parent = os.Worker?.parent;
+let pool =new ProcessPool();
 
-const log = (...args) => console.log('WORKER', ...args);
+//const log = (...args) => console.log('WORKER', ...args);
 
 console.log('parent',parent);
+
+parent.onmessage = msg => {
+  console.log('Message:',msg);
+  const {data}=msg;
+  switch(data.type) {
+    case 'exec': 
+    let child=pool.start(...data.args);
+    child.then(r => {
+      //console.log('child:',r);
+
+      parent.postMessage({ type: 'wait', child: r});
+    });
+
+    break;
+  }
+}
 
 parent.postMessage('test');
 `);
@@ -38,42 +62,44 @@ parent.postMessage('test');
   worker.onmessage = HandleMessage;
   // console.log('worker', Object.getOwnPropertyNames(Object.getPrototypeOf(worker)).reduce((acc, n) => ({ ...acc, [n]: worker[n] }), {}));
   console.log('TestWorker', worker.onmessage);
+  console.log('TestWorker', worker.postMessage);
+
+  worker.postMessage({ type: 'exec', args: ['ls', '-la'] });
 
   os.setReadHandler(0, () => {
     let line = process.stdin.getline();
 
     worker.postMessage({ line });
   });
-}
 
-function HandleMessage(e) {
-  console.log('HandleMessage', e);
-  var ev = e.data;
-  switch (ev.type) {
-    case 'num':
-      assert(ev.num, counter);
-      counter++;
-      if(counter == 10) {
-        /* test SharedArrayBuffer modification */
-        let sab = new SharedArrayBuffer(10);
-        let buf = new Uint8Array(sab);
-        worker.postMessage({ type: 'sab', buf: buf });
-        counter = 0;
-      }
-      break;
-    case 'sab_done':
-      {
-        let buf = ev.buf;
-        /* check that the SharedArrayBuffer was modified */
-        assert(buf[2], 10);
-        worker.postMessage({ type: 'abort' });
-      }
-      break;
-    case 'done':
-      /* terminate */
-      // worker.onmessage = null;
-      break;
+  function HandleMessage(e) {
+    console.log('HandleMessage', e);
+    var ev = e.data;
+    switch (ev.type) {
+      case 'num':
+        assert(ev.num, counter);
+        counter++;
+        if(counter == 10) {
+          /* test SharedArrayBuffer modification */
+          let sab = new SharedArrayBuffer(10);
+          let buf = new Uint8Array(sab);
+          worker.postMessage({ type: 'sab', buf: buf });
+          counter = 0;
+        }
+        break;
+      case 'sab_done':
+        {
+          let buf = ev.buf;
+          /* check that the SharedArrayBuffer was modified */
+          assert(buf[2], 10);
+          worker.postMessage({ type: 'abort' });
+        }
+        break;
+      case 'done':
+        /* terminate */
+        // worker.onmessage = null;
+        break;
+    }
   }
 }
-
 TestWorker();
