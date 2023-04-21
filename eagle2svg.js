3e383956e651aa6089d1a2adbdf5d5604ded5c2f
Roman Senn <roman.l.senn@gmail.com>
2023-04-21 17:49:55 +0000

    ...

--- eagle2svg.js
+++ eagle2svg.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env qjsm
-import * as util from './lib/misc.js';
-import * as deep from './lib/deep.js';
+
+
 import * as path from './lib/path.js';
-import * as fs from 'fs';
+
 import { log, EagleDocument, Renderer, EagleProject } from './lib/eagle.js';
-import require from 'require';
+
 import { Console } from 'console';
-import { read as fromXML, write as toXML } from './lib/xml.js';
+
 import { WriteFile } from './io-helpers.js';
 import { getOpt } from 'util';
 import { readFileSync } from 'fs';
 import { ReactComponent, render } from './lib/dom/preactComponent.js';
 import renderToString from './lib/preact-render-to-string.js';
-import { RGBA, isRGBA, ImmutableRGBA, default as rgba } from './lib/color/rgba.js';
+
 
 let debugFlag = false;
 
@@ -77,7 +77,7 @@ function main(...args) {
   );
 
   for(let arg of args) {
-    let doc = EagleDocument.open(arg, { readFileSync });
+    let doc = EagleDocument.open(arg, f =>  readFileSync(f, 'utf-8'));
     let file = path.basename(doc.filename, '.' + doc.type) + '-' + { sch: 'schematic', brd: 'board', lbr: 'library' }[doc.type] + '.svg';
 
     if(params['output-dir']) file = path.join(params['output-dir'], file);
