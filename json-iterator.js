1dfbaa85b0480b9d1942eeee632f65bbbb64d0ca
Roman Senn <roman.l.senn@gmail.com>
2023-05-25 21:25:45 +0200

    ...

--- json-iterator.js
+++ json-iterator.js
@@ -6,34 +6,72 @@ export function* JSON_Iterator(obj, pred = (v, k) => true) {
   let parent;
   let str = '';
   const nl = () => '\n' + ' '.repeat(stack.length * 2);
-  const push = {
-    array: () => (stack.push([']', parent]), (parent = 'array'), (str += '[' + nl())),
-    object: () => (stack.push(['}', parent]), (parent = 'object'), (str += '{' + nl()))
-  };
+  /*const push = {
+    array() {
+      let obj;
+      stack.push(obj=[']', parent]);
+      parent = 'array';
+      str += '[' + nl();
+      return obj;
+    },
+    object() {
+      let obj;
+      stack.push(obj=['}', parent]);
+      parent = 'object';
+      str += '{' + nl();
+      return obj;
+    }
+  };*/
 
-  push[prev.type]();
+  //ush[prev.type]();
+  stack[0] = ['}', 'object'];
 
   for(let [value, path] of iterate(obj, pred, RETURN_VALUE_PATH)) {
     const depth = path.length;
     const key = path[depth - 1];
     const type = valueType(value);
     let c = common(path, prev.path);
-    let descend = prev.depth - c,
-      ascend = depth - c;
-    let d = descend ? -(descend - 1) : ascend;
+    const descend = path.length - c;
+    const ascend = prev.path.length - c;
+    const delta = descend - ascend;
+    const keys = { ascend: prev.path.slice(c), descend: path.slice(c) };
 
-    if(d < 0)
-      for(let i = d; i < 0; i++) {
-        let [tmp, p] = stack.pop();
-        str += nl() + tmp;
-        parent = p;
-      }
-    if(d <= 0) str += ',' + nl();
+    //   while(stack.length > c) {
+    for(let i = 0; i < ascend - 1; ++i) {
+      let [tmp, p] = stack.pop();
+      str += nl() + tmp;
+      parent = p;
+    }
+
+    if(delta <= 0) str += ',' + nl();
+
+    const isnum = n => !isNaN(+n);
+    const m = [
+      ['{', '}', 'object'],
+      ['[', ']', 'array']
+    ];
+
+    for(let i = c; i < path.length; ++i) {
+      let [open, close, name] = m[isnum(path[i]) | 0];
+
+      str += open+nl();
+      stack[ i] = [close, m[isnum(path[ i-1]) | 0][2], path[ i-1]];
+      parent = name;
+
+      //   push[isnum(path[c+i]) ? 'array' : 'object']()[2]=path[c+i];
+    }
 
     if(/^[A-Za-z_$][A-Za-z_$0-9]*$/.test(key)) str += key + ':';
     else if(parent != 'array') str += JSON.stringify(key) + ':';
 
-    if(push[type]) push[type]();
+    /*switch(type) {
+  case 'object':str+='{'+nl(); stack[depth]=['}', parent]; break;
+  case 'array': str+='['+nl(); stack[depth]=[']', parent]; break;
+case 'null':str += 'null'; break;
+default: 
+}*/
+    if(type == 'object' || type == 'array') {} 
+    else if(type == 'null' || value === null) str += 'null';
     else str += JSON.stringify(value).trim();
 
     if(str.length >= 2) {
@@ -41,11 +79,24 @@ export function* JSON_Iterator(obj, pred = (v, k) => true) {
       str = '';
     }
 
+    console.log('JSON_Iterator', console.config({ compact: 3, depth: 4 }), {
+      // delta: `-${descend}/+${ascend} = ${((delta > 0 ? '+' : '') + delta).padStart(2)} -> ${depth}`,
+      type,
+      depth,
+      ascend,
+      descend,
+      delta,
+      stackLen: stack.length,
+      path: s(path),
+      stack,
+      c
+    });
+
     prev = { depth, path, type };
   }
   while(stack.length) {
     let [tmp, p] = stack.pop();
-    str += tmp;
+    str += nl() + tmp;
   }
   if(str.length >= 0) yield str;
 
@@ -59,6 +110,6 @@ export function* JSON_Iterator(obj, pred = (v, k) => true) {
     return n;
   }
   function valueType(value) {
-    return Array.isArray(value) ? 'array' : typeof value;
+    return Array.isArray(value) ? 'array' : value !== null ? typeof value : 'null';
   }
 }
