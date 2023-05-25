12bd32c516f2cf595e6617206fc583ae5cc96971
Roman Senn <roman.l.senn@gmail.com>
2023-05-25 23:18:47 +0200

    ...

---
+++ test-matshared.js
@@ -0,0 +1,14 @@
+import { Mat, CV_8UC4 } from 'opencv';
+import { toPointer } from 'util';
+
+let buf = new SharedArrayBuffer(640 * 480 * 4);
+
+console.log('toPointer(buf)', toPointer(buf));
+
+let m = new Mat(480, 640, CV_8UC4, buf);
+const { elemSize, elemSize1 } = m;
+
+console.log('m', m);
+console.log('m.buffer', m.buffer);
+console.log('toPointer(m.buffer)', toPointer(m.buffer));
+console.log('m', { elemSize, elemSize1 });
