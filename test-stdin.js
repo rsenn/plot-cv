ba8fb4d27aa0feacaff40e502285193840c79ae4
Roman Senn <roman.l.senn@gmail.com>
2023-06-16 19:25:59 +0200

    ...

---
+++ test-stdin.js
@@ -0,0 +1,9 @@
+import * as os from 'os';
+import * as std from 'std';
+
+os.ttySetRaw(0);
+
+let ab = new ArrayBuffer(1024);
+let r = std.in.read(ab, 0, 1024);
+
+console.log('data:', r >= 0 ? ab.slice(0, r) : null);
