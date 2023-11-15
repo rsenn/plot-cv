0495fa4ee3734e121d04fc4f6950e4d8bd152f25
Roman Senn <roman.l.senn@gmail.com>
2023-11-15 09:52:21 +0100

    ...

--- tokmap.c
+++ tokmap.c
@@ -29,28 +29,10 @@
 #define TTENT(X, Y) [TTINT(X)] = Y
 
 static const int bplist[] = {
-  TTENT(TT_LOR, 1 << 4),
-  TTENT(TT_LAND, 1 << 5),
-  TTENT(TT_BOR, 1 << 6),
-  TTENT(TT_XOR, 1 << 7),
-  TTENT(TT_BAND, 1 << 8),
-  TTENT(TT_EQ, 1 << 9),
-  TTENT(TT_NEQ, 1 << 9),
-  TTENT(TT_LTE, 1 << 10),
-  TTENT(TT_GTE, 1 << 10),
-  TTENT(TT_LT, 1 << 10),
-  TTENT(TT_GT, 1 << 10),
-  TTENT(TT_SHL, 1 << 11),
-  TTENT(TT_SHR, 1 << 11),
-  TTENT(TT_PLUS, 1 << 12),
-  TTENT(TT_MINUS, 1 << 12),
-  TTENT(TT_MUL, 1 << 13),
-  TTENT(TT_DIV, 1 << 13),
-  TTENT(TT_MOD, 1 << 13),
-  TTENT(TT_NEG, 1 << 14),
-  TTENT(TT_LNOT, 1 << 14),
-  TTENT(TT_LPAREN, 1 << 15),
-  TTENT(TT_RPAREN, 0),
+    TTENT(TT_LOR, 1 << 4),  TTENT(TT_LAND, 1 << 5),  TTENT(TT_BOR, 1 << 6),     TTENT(TT_XOR, 1 << 7),  TTENT(TT_BAND, 1 << 8), TTENT(TT_EQ, 1 << 9),
+    TTENT(TT_NEQ, 1 << 9),  TTENT(TT_LTE, 1 << 10),  TTENT(TT_GTE, 1 << 10),    TTENT(TT_LT, 1 << 10),  TTENT(TT_GT, 1 << 10),  TTENT(TT_SHL, 1 << 11),
+    TTENT(TT_SHR, 1 << 11), TTENT(TT_PLUS, 1 << 12), TTENT(TT_MINUS, 1 << 12),  TTENT(TT_MUL, 1 << 13), TTENT(TT_DIV, 1 << 13), TTENT(TT_MOD, 1 << 13),
+    TTENT(TT_NEG, 1 << 14), TTENT(TT_LNOT, 1 << 14), TTENT(TT_LPAREN, 1 << 15), TTENT(TT_RPAREN, 0),
 };
 
 int
