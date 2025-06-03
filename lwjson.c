eca234716b76b369764c778d6fb9dc26e140e185
Roman Senn <roman.l.senn@gmail.com>
2025-06-03 22:28:10 +0200

    ...

---
+++ lwjson.c
@@ -0,0 +1,2 @@
+#include "../json-parser/lwjson/lwjson/src/lwjson/lwjson.c"
+#include "../json-parser/lwjson/lwjson/src/lwjson/lwjson_stream.c"
---
+++ wasm3.c
@@ -0,0 +1,7 @@
+#include "quickjs/qjs-modules/third_party/wasm3/source/m3_api_wasi.h"
+#include "quickjs/qjs-modules/third_party/wasm3/source/wasm3.h"
+#include "quickjs/qjs-modules/third_party/wasm3/source/m3_env.h"
+
+int
+main() {
+}
