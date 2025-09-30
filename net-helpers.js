841830bfd50c326d01442bde978f6b2f4162fdf0
Roman Senn <roman.l.senn@gmail.com>
2025-09-30 04:11:31 +0200

    ...

--- net-helpers.js
+++ net-helpers.js
@@ -1,5 +1,8 @@
 import { fetch } from 'fetch';
+import { define } from 'util';
 
 export async function httpGet(url, opts = {}) {
-  return await (await fetch(url, { tls: { key: 'localhost.key', cert: 'localhost.crt', ca: 'ca.crt', rejectUnauthorized: false }, h2: false, ...opts })).text();
+  const resp = await fetch(url, { tls: { key: 'localhost.key', cert: 'localhost.crt', ca: 'ca.crt', rejectUnauthorized: false }, h2: false, ...opts });
+
+  return await resp.text();
 }
