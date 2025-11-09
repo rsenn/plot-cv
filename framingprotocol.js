f0027ee4f3e69efc96b4b332fca30bfef058925e
Roman Senn <roman.l.senn@gmail.com>
2025-11-09 14:02:59 +0100

    ...

--- framingprotocol.js
+++ framingprotocol.js
@@ -1,41 +1,66 @@
 import { AsyncSocket } from 'sockets';
 import { TextDecoder } from 'textcode';
-import { define, wrapFunction } from 'util';
+import { extend, define, properties, nonenumerable, declare, assign, wrapFunction } from 'util';
 import extendFunction from 'extendFunction';
 
 extendFunction();
 
 export async function connect(host = '127.0.0.1', port = 9999) {
   const sock = new AsyncSocket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+  const lenBuf = new ArrayBuffer(9);
 
-  define(sock, {
-    readBuf: [
-      async (call, [n, ab]) => {
-        ab ??= new ArrayBuffer(n);
-        return (await call(ab, n)) ? ab : null;
-      },
-      async (call, [n, ab]) => {
-        if((ab = await call(n, ab))) return new TextDecoder().decode(ab.slice(0, n));
-      },
-      async call => await call(+('0x' + (await call(9)))),
-      async call => JSON.parse(await call()),
-    ].reduce(wrapFunction, readFully(sock.recv)),
+  let id,
+    obj = Object.setPrototypeOf({}, { [Symbol.toStringTag]: 'FramingProtocol' });
+
+  const encode = (
+    (e = new TextEncoder()) =>
+    s =>
+      e.encode(s)?.buffer
+  )();
+
+  extend(obj, {
+    read: [
+      async (call, [n, ab = new ArrayBuffer(n)]) => (await call(ab, n)) && ab,
+      async (call, [n, ab]) => (ab = await call(n, ab)) && new TextDecoder().decode(ab.slice(0, n)),
+      async call => await call(+('0x' + (await call(9, lenBuf)))),
+      async call => JSON.parse((await call()) || 'null'),
+    ].reduce(wrapFunction, readFully),
   });
 
-  await sock.connect(host, port);
-  return sock;
-}
+  extend(obj, {
+    async *[Symbol.asyncIterator]() {
+      let data;
+      while((data = await this.read())) yield data;
+    },
+  });
 
-export function readFully(fn, thisObj) {
-  return async function(buf, length) {
+  extend(obj, {
+    sendMessage: [
+      async (send, [b]) => (await send(b.byteLength.toString(16).padStart(8, '0') + '\n')) + (await send(b)),
+      (send, [msg]) => send(encode(JSON.stringify(msg))),
+    ].reduce(wrapFunction, (buf, length = buf.byteLength) => sock.send(buf, 0, length)),
+  });
+
+  extend(obj, {
+    sendRequest: [
+      (message, [request]) => message({ type: 'request', request }),
+      async (request, [command, args], request_seq = (id = (id ?? 0) + 1)) =>
+        (await request({ command, args, request_seq })) && request_seq,
+    ].reduce(wrapFunction, obj.sendMessage),
+  });
+
+  async function readFully(buf, length) {
     let offset = 0;
 
     while(offset < length) {
-      let received = await fn.call(thisObj ?? this, buf, offset, length - offset);
+      const received = await sock.recv(buf, offset, length - offset);
       if(received <= 0) return 0;
       offset += received;
     }
 
     return 1;
-  };
+  }
+
+  await sock.connect(host, port);
+  return obj;
 }
--- test-opencv.js
+++ test-opencv.js
@@ -238,7 +238,11 @@ async function main(...args) {
   let backgroundColor = 0xd0d0d0;
   let shadowColor = 0x404040;
   let textColor = 0xd3d7cf;
-  let fonts = ['/home/roman/.fonts/gothic.ttf', '/home/roman/.fonts/gothicb.ttf', '/usr/share/fonts/truetype/ubuntu/UbuntuMono-R.ttf'];
+  let fonts = [
+    '/home/roman/.fonts/gothic.ttf',
+    '/home/roman/.fonts/gothicb.ttf',
+    '/usr/share/fonts/truetype/ubuntu/UbuntuMono-R.ttf',
+  ];
   let fontFace = fonts[2];
   let fontSize = 14;
 
@@ -277,7 +281,8 @@ async function main(...args) {
   let paramIndexes = [-1, -1];
   let palette = new Array();
   const black = [0x00, 0x00, 0x00, 0xff];
-  for(let i = 0; i < 8; i++) palette[i] = [i & 0x04 ? 0xff : 0x00, i & 0x02 ? 0xff : 0x00, i & 0x01 ? 0xff : 0x00, 0xff];
+  for(let i = 0; i < 8; i++)
+    palette[i] = [i & 0x04 ? 0xff : 0x00, i & 0x02 ? 0xff : 0x00, i & 0x01 ? 0xff : 0x00, 0xff];
   palette[2] = [0x60, 0x60, 0x60, 0xff];
   palette[3] = [0xff, 0xff, 0x0, 0xff];
   for(let i = 8; i < 16; i++) palette[i] = black;
@@ -302,7 +307,10 @@ async function main(...args) {
         cv.threshold(src, dst, +params.thres, 255, +params.type);
       },
       function Morphology(src, dst) {
-        let structuringElement = cv.getStructuringElement(cv.MORPH_CROSS, new Size(+params.kernel_size * 2 + 1, +params.kernel_size * 2 + 1));
+        let structuringElement = cv.getStructuringElement(
+          cv.MORPH_CROSS,
+          new Size(+params.kernel_size * 2 + 1, +params.kernel_size * 2 + 1),
+        );
         src.copyTo(dst);
         cv.morphologyEx(dst, dst, cv.MORPH_ERODE, structuringElement);
         dst.xor([255, 255, 255, 0], dst);
@@ -312,7 +320,12 @@ async function main(...args) {
         console.log('Skeletonization', dst);
       },
       function SkeletonTracing(src, dst) {
-        let count = cv.traceSkeleton(src, (contours = globalThis.contours = []), (neighborhood = globalThis.neighborhood = new Mat()), (mapping = globalThis.mapping = new Mat()));
+        let count = cv.traceSkeleton(
+          src,
+          (contours = globalThis.contours = []),
+          (neighborhood = globalThis.neighborhood = new Mat()),
+          (mapping = globalThis.mapping = new Mat()),
+        );
 
         console.log('SkeletonTracing', console.config({ maxArrayLength: 5, depth: 3, compact: 2 }), {
           count,
@@ -412,7 +425,15 @@ async function main(...args) {
         let output = new Mat();
         if(skel.channels > 1) cv.cvtColor(skel, skel, cv.COLOR_BGR2GRAY);
         if(morpho.channels > 1) cv.cvtColor(morpho, morpho, cv.COLOR_BGR2GRAY);
-        cv.HoughLinesP(skel, output, +params.rho, (Math.PI * (+params.theta || 1)) / 180, +params.threshold, +params.minLineLength, +params.maxLineGap);
+        cv.HoughLinesP(
+          skel,
+          output,
+          +params.rho,
+          (Math.PI * (+params.theta || 1)) / 180,
+          +params.threshold,
+          +params.minLineLength,
+          +params.maxLineGap,
+        );
         cv.cvtColor(skel, dst, cv.COLOR_GRAY2BGR);
         let i = 0;
         lines.splice(0, lines.length);
@@ -488,7 +509,14 @@ async function main(...args) {
       function HoughCircles(src, dst) {
         const morpho = this.outputOf('Morphology');
         const skel = this.outputOf('Skeletonization');
-        const paramArray = [+params.dp || 1, +params.minDist, +params.param1, +params.param2, +params.minRadius, +params.maxRadiMathus];
+        const paramArray = [
+          +params.dp || 1,
+          +params.minDist,
+          +params.param1,
+          +params.param2,
+          +params.minRadius,
+          +params.maxRadiMathus,
+        ];
         let circles1 = [] || new Mat();
         let circles2 = [] || new Mat();
         cv.HoughCircles(morpho, circles1, cv.HOUGH_GRADIENT, ...paramArray);
@@ -599,7 +627,8 @@ async function main(...args) {
   function keyhandler(event) {
     console.log('onkey', event);
     const { scancode } = event;
-    if(scancode === 'q' || scancode === 113 || scancode === '\x1b' || scancode === 0x100071 || scancode === -1) running = false;
+    if(scancode === 'q' || scancode === 113 || scancode === '\x1b' || scancode === 0x100071 || scancode === -1)
+      running = false;
     switch (scancode & 0xfff) {
       case 0xf08 /* backspace */:
       case 0x08 /* backspace */:
