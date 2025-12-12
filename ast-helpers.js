3c6d406dc642bda256c1692de6ea8c6aaac4c1ad
Roman Senn <roman.l.senn@gmail.com>
2025-12-12 02:10:53 +0100

    ...

--- ast-helpers.js
+++ ast-helpers.js
@@ -1,21 +1,55 @@
-import { get, select, iterate, RETURN_PATH_VALUE, RETURN_VALUE_PATH, RETURN_VALUE, PATH_AS_POINTER, FILTER_HAS_KEY, TYPE_OBJECT } from 'deep';
+import { get, select, iterate, RETURN_PATH_VALUE, RETURN_VALUE_PATH, RETURN_VALUE, RETURN_PATH, PATH_AS_POINTER, FILTER_HAS_KEY, TYPE_OBJECT } from 'deep';
 import { Pointer } from 'pointer';
 import { readFileSync } from 'fs';
-import { isObject, isFunction, isString, isNumeric, types, weakMapper } from 'util';
+import { isObject, isFunction, isString, isNumeric, types, mapFunction, weakMapper } from 'util';
 
-export function GetQualifier(ast, path) {
-  const ptr = new Pointer(path);
+export const TreeSource = mapFunction(new WeakMap());
+export const SourceLines = weakMapper(source => ('\n' + readFileSync(source, 'utf-8').trimEnd()).split(/\n/g), new Map());
+
+export const NodeTree = mapFunction(new WeakMap());
+export const NodePath = mapFunction(new WeakMap());
+
+export function MakePointer(p) {
+  return Pointer.isPointer(p) ? p : new Pointer(p);
+}
+
+export function GetNodeArguments(a) {
+  if(Array.isArray(a) && Array.isArray(a[0]) && a[0].length == 2) a = [...a.shift()];
+
+  let ast, ptr;
+
+  if(a.length >= 1 && !a[1]) {
+    const node = a.shift();
+
+    ptr = NodePath(node);
+    ast = NodeTree(node);
+  }
+
+  if(!ast && !ptr)
+    if(a.length >= 2 && a[1]) {
+      [ast, ptr] = a.splice(0, 2);
+    }
+
+  ast = NodeTree(ast) ?? ast;
+  ptr = [...ptr];
+
+  if(ast && ptr) return [ast, ptr];
+}
+
+export function GetQualifier(...args) {
   const a = [];
-  for(const [p, n] of ptr.hier(p => [p, p.deref(ast)]).filter(([p, n]) => 'type' in n)) {
-    //if('id' in n)
-    a.push([n.type, n.id?.name ?? n.key?.name]);
+
+  for(const [n, p] of WalkDown(...args)) {
+    const part = n.id?.name ?? n.key?.name;
+
+    /*if(part)*/ a.push([part, n, p]);
   }
 
   return a;
 }
 
-export function IterateNodes(root, pred) {
-  return iterate(root, pred, RETURN_VALUE_PATH | FILTER_HAS_KEY, TYPE_OBJECT, ['type']);
+export function* IterateNodes(root, pred) {
+  for(const [n, p] of iterate(root, NodePredicate(pred), RETURN_VALUE_PATH | FILTER_HAS_KEY, TYPE_OBJECT, ['type'])) yield GetNodePath(root, p);
 }
 
 export function* GetImportSpecifiers(root) {
@@ -23,7 +57,7 @@ export function* GetImportSpecifiers(root) {
 }
 
 export function IsInsideAny(ast, path, pred) {
-  for(const [n, p] of WalkUp(ast, path)) if(pred(n, p, ast)) return true;
+  for(const [n, p] of WalkUp(ast, path)) if(NodePredicate(pred)(n, p, ast)) return true;
   return false;
 }
 
@@ -31,24 +65,44 @@ export function ParentPath(path) {
   return new Pointer(path).slice(0, isNumeric(path[path.length - 1]) ? -2 : -1);
 }
 
-export function ParentNode(ast, path) {
+export function GetParentNodePath(...args) {
   let i = 0;
-  for(let [n, p] of WalkUp(ast, path)) {
-    if(i) {
-      if(isObject(n) && n.type) return [n, p];
-    }
 
-    ++i;
-  }
+  for(const [n, p] of WalkUp(...args)) if(i++) return [n, p];
 }
 
-const sourceFiles = weakMapper(source => readFileSync(source, 'utf-8').trimEnd().split(/\n/g), new Map());
-
-export function GetSourceLine(source, line) {
-  return sourceFiles(source)[line - 1];
+export function SourceRange(source, range) {
+  return SourceLines(source)
+    .slice(1)
+    .join('\n')
+    .substring(...range);
 }
 
-export function Predicate(pred) {
+export function GetNode(ast, path) {
+  const node = get(ast, path);
+  const ptr = NodePath(node) ?? new Pointer(path);
+  NodeTree(node, ast);
+  NodePath(node, ptr);
+  return node;
+}
+
+export function GetNodePath(ast, path) {
+  const node = get(ast, path);
+  const ptr = NodePath(node) ?? new Pointer(path);
+  NodeTree(node, ast);
+  NodePath(node, ptr);
+  return [node, ptr];
+}
+
+export function GetPath(ast, path) {
+  const node = get(ast, path);
+  const ptr = NodePath(node) ?? new Pointer(path);
+  NodeTree(node, ast);
+  NodePath(node, ptr);
+  return ptr;
+}
+
+export function NodePredicate(pred) {
   if(isString(pred)) {
     const s = pred;
     pred = n => n.type == s;
@@ -56,35 +110,42 @@ export function Predicate(pred) {
     const re = pred;
     pred = n => re.test(n.type);
   }
-
-  if(!isFunction(pred)) pred = () => true;
-
+  if(!pred) pred = () => true;
   return pred;
 }
 
 export function IsInside(ast, path, pred) {
-  const [n, p] = ParentNode(ast, path);
-  return Predicate(pred)(n, p, ast);
+  const [n, p] = GetParentNodePath(ast, path);
+  return NodePredicate(pred)(n, p, ast);
 }
 
-export function* WalkUp(ast, path) {
-  const n = path.length;
-  for(let i = n; i >= 0; i--) {
-    const p = path.slice(0, i);
-    yield [get(ast, p), p];
+export function* WalkUp(...args) {
+  const [ast, path] = GetNodeArguments(args);
+
+  //path = [...path];
+  const pred = NodePredicate(args.shift() ?? (n => !Array.isArray(n)));
+
+  for(let i = path.length; i >= 0; i--) {
+    const node_path = GetNodePath(ast, path.slice(0, i));
+    if(pred(...node_path, ast)) yield node_path;
   }
 }
 
-export function* WalkDown(ast, path) {
-  const n = path.length;
-  for(let i = 0; i <= n; i++) {
-    const p = path.slice(0, i);
-    yield [get(ast, p), p];
+export function* WalkDown(...args) {
+  const [ast, path] = GetNodeArguments(args);
+
+  const pred = NodePredicate(args.shift() ?? (n => !Array.isArray(n)));
+
+  const { length } = path;
+
+  for(let i = 0; i <= length; i++) {
+    const node_path = GetNodePath(ast, path.slice(0, i));
+    if(pred(...node_path, ast)) yield node_path;
   }
 }
 
-export function* GetNodeTypes(ast, path) {
-  for(const [n, p] of WalkDown(ast, path)) if(isObject(n) && n.type) yield [n.type, p];
+export function* GetNodeTypes(...args) {
+  for(const [n, p] of WalkDown(...args)) if(isObject(n) && n.type) yield [n.type, p];
 }
 
 export function GetImportIdentifiers(root) {
@@ -97,10 +158,10 @@ export function GetImportIdentifiers(root) {
 }
 
 export function* GetNonImportIdentifiers(root) {
-  for(const [id, path] of IterateNodes(root, e => e.type == 'Identifier')) {
-    if(IsInsideAny(root, path, e => /Import.*Decl/.test(e.type))) continue;
+  for(const [id, path] of IterateNodes(root, 'Identifier')) {
+    if(IsInsideAny(root, path, /Import.*Decl/)) continue;
 
-    if(path[path.length - 1] == 'property') continue;
+    //    if(path[path.length - 1] == 'property') continue;
 
     yield [id.name, new Pointer(path)];
   }
