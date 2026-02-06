eae678d2c1504c1cf5fa06baaf1ced49b07b91a3
Roman Senn <roman.l.senn@gmail.com>
2026-02-06 23:47:02 +0100

    ...

--- images2video.js
+++ images2video.js
@@ -1,24 +1,30 @@
-import { Mat, CV_8UC3, Rect, Point, Size, VideoWriter, imread, resize } from 'opencv';
+import { Mat, CV_8UC3, Rect, Point, Size, CAP_FFMPEG,CAP_ANDROID, CAP_GSTREAMER,  VideoWriter, imread, resize } from 'opencv';
 import { readFileSync } from 'fs';
 import { toString, getOpt } from 'util';
 
 function main(...args) {
-  let { fps=25, output = process.env.HOME + '/storage/movies/output.mp4', ...params } = getOpt(
+  let {
+    fps = 25,
+    fourcc = 'MPEG',
+    output = process.env.HOME + '/storage/movies/output.mp4',
+    ...params
+  } = getOpt(
     {
       verbose: [false, (a, v) => (v | 0) + 1, 'v'],
       output: [true, null, 'o'],
       fps: [true, null, 'f'],
+      fourcc: [true, null, 'c'],
       '@': 'images.../c',
     },
     args,
   );
 
   const size = new Size(1080, 1440);
-  const w = new VideoWriter(output, VideoWriter.fourcc('avc1'), fps, size);
+  const w = new VideoWriter(output, CAP_FFMPEG, VideoWriter.fourcc(fourcc), fps, size);
 
   const frame = new Mat(size, CV_8UC3);
   const { length } = params['@'];
-  
+
   let i = 0;
 
   for(const image of params['@']) {
@@ -55,4 +61,5 @@ function main(...args) {
   w.release();
 }
 
-main(...scriptArgs.slice(1));
+try {                                                            main(...scriptArgs.slice(1));                                } catch(error) {                                                 console.log(`FAIL: ${error?.message ?? error}\n${error?.stack}`);
+}
