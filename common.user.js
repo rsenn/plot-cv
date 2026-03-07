83f219ddf99c4485af2c8e71de968f17b55bec7f
Roman Senn <roman.l.senn@gmail.com>
2026-03-07 04:11:06 +0100

    ...

--- common.user.js
+++ common.user.js
@@ -14,10 +14,16 @@
 (function () {
   'use strict';
 
-   console.log('Common Userscript LOADED');
+  console.log('Common Userscript LOADED');
 
   Object.assign(window, {
-    waitFor: ms => new Promise((r) => setTimeout(r, ms)),
+    concat: (...args) => Array.prototype.concat.call(...args),
+    applyGen: (it, fn) => fn(...it),
+    *protoChain(o, t = a => a) {
+      do yield t(o);
+      while((o = Object.getPrototypeOf(o)));
+    },
+    waitFor: ms => new Promise(r => setTimeout(r, ms)),
     Q: (s, d = document) => d.querySelector(s),
     QA: (s, d = document) => [...(d.querySelectorAll(s) ?? [])],
   });
