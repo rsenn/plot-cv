73c22ed7f93961ae128cd8f9d89c6dc155f6e442
Roman Senn <roman.l.senn@gmail.com>
2026-03-20 19:21:39 +0100

    ...

--- splitdiff.js
+++ splitdiff.js
@@ -3,34 +3,52 @@ import { readFileSync, writeFileSync } from 'fs';
 import { getOpt } from 'util';
 import { basename, dirname, extname } from 'path';
 
+let noWhitespace = false;
+
 function FormatName(name) {
-  return name.replaceAll(/[\W]/g, '_');
+  return name.replaceAll(/[\W]/g, '-');
+}
+
+function ChunkIsWhitespace(chunk) {
+  const { content, changes } = chunk;
+
+  for(const { type, content } of changes) {
+    switch (type) {
+      case 'add':
+      case 'del': {
+        if(content.slice(1).trim().length > 0) return false;
+        break;
+      }
+    }
+  }
+
+  return true;
 }
 
 function Chunk(chunk) {
   const { content, changes } = chunk;
-  let s = '';
+  let s = content + '\n';
 
-  s += content + '\n';
-
-  for(let { content } of changes) {
-    s += content + '\n';
-  }
+  for(const { content } of changes) s += content + '\n';
 
   return s;
 }
 
 function ProcessDiff(d, filename) {
-  let base = basename(filename, extname(filename));
-  let i = 1;
+  const base = basename(filename, extname(filename));
 
-  for(let diff of d) {
+  for(const diff of d) {
     const { chunks, from: src, to: dst } = diff;
 
-    let s = `diff --git a/${src} b/${dst}\n--- a/${src}\n+++ b/${dst}\n`;
+    let i = 1,
+      s = `diff --git a/${src} b/${dst}\n--- a/${src}\n+++ b/${dst}\n`;
 
-    for(let chunk of chunks) {
-      writeFileSync(base + '-' + FormatName(dst) + `-${i++}.diff`, s + Chunk(chunk));
+    const n = Math.ceil(Math.log10(chunks.length) + 0.0001);
+
+    for(const chunk of chunks) {
+      if(noWhitespace && ChunkIsWhitespace(chunk)) continue;
+
+      writeFileSync(base + '_' + FormatName(dst) + '_' + `${i++}`.padStart(n, '0') + `.diff`, s + Chunk(chunk));
     }
   }
 }
@@ -39,12 +57,13 @@ function main(...args) {
   let params = (globalThis.params = getOpt(
     {
       verbose: [false, (a, v) => (v | 0) + 1, 'v'],
+      'no-whitespace': [false, () => (noWhitespace = true), 'W'],
       '@': 'files',
     },
     args,
   ));
 
-  for(let arg of params['@']) {
+  for(const arg of params['@']) {
     const d = parseDiff(readFileSync(arg, 'utf-8'));
 
     console.log(arg, console.config({ compact: false }), d);
