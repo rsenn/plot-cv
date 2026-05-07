cb04ae833277d320275ce6a234a525a7b2ea7f5a
Roman Senn <roman.l.senn@gmail.com>
2026-05-07 20:37:47 +0200

    ...

---
+++ crosswindow.cpp
@@ -0,0 +1,4 @@
+#include "../CrossWindow/src/CrossWindow/Common/Event.cpp"
+#include "../CrossWindow/src/CrossWindow/Common/Init.cpp"
+#include "../CrossWindow/src/CrossWindow/XCB/XCBEventQueue.cpp"
+#include "../CrossWindow/src/CrossWindow/XCB/XCBWindow.cpp"
