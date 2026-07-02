1087a79262d27c75fcefdbc41b124ba13824af06
Roman Senn <roman.l.senn@gmail.com>
2026-07-03 00:05:18 +0200

    ...

--- coinbase-ec-key.sh
+++
@@ -1,2 +0,0 @@
-export COINBASE_API_KEY="organizations/4a4f732b-dc30-4d55-b269-4d122f73fef8/apiKeys/ba40a41c-c92f-4e72-9892-db45b46a54d9"
-export COINBASE_API_SECRET="-----BEGIN EC PRIVATE KEY-----\nMHcCAQEEIDVZFLStGhhBSSPG6dev/VH0kXhEaj4nmnevojqXfatDoAoGCCqGSM49\nAwEHoUQDQgAEa0NimxowYETlp/IWzYEkcziatJoQuhjp4rWwueCMeuLthao9Ycaa\nv91yAs9D+P/Q3BfC611+e6nsOw+3AzWtcA==\n-----END EC PRIVATE KEY-----\n"
